301f670734f809ff078bb1c31e9d7f3ddf8f3fa9
Peter Gaultney <petergaultney@gmail.com>
2023-02-15 10:23:16 -0600

    re-lock all projects from dbxtend downstream
    GitOrigin-RevId: 5a118f351d853b1325d16024dab46fab3a231479

--- libs/core/src/thds/core/stack_config.py
+++
@@ -1,7 +0,0 @@
-"""The purpose of this module is to allow overlaying a base config
-with StackContext, such that configuration can be overridden on a
-per-thread basis within a running application.
-
-Essentially this is an extension of the idea of dependency injection
-to the concept of config.
-"""
