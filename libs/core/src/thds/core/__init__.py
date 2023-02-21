8b47128408f3d0636fef184623bd461386ee537b
Peter Gaultney <petergaultney@gmail.com>
2023-02-21 14:54:30 -0600

    try dash-separated namespace for thds-core
    GitOrigin-RevId: a700db371b357c756169ff2bff530efdf3969879

--- libs/core/src/thds/core/__init__.py
+++ libs/core/src/thds/core/__init__.py
@@ -1,5 +1,4 @@
 """Trilliant Health data science team core utils"""
-import sys
 from importlib.metadata import PackageNotFoundError, version
 
 from .meta import read_metadata
@@ -8,13 +7,7 @@ try:
     __version__ = version(__name__)
     assert __version__, f"if package {__name__} is found, version should exist."
 except PackageNotFoundError:  # pragma: no cover
-    sys.stderr.write(f"no version found for {__name__}")
-    try:
-        under_name = __name__.replace(".", "_")
-        __version__ = version(under_name)
-    except PackageNotFoundError:
-        sys.stderr.write(f"no version found for {under_name}")
-        __version__ = ""
+    __version__ = ""
 
 metadata = read_metadata(__name__)
 __basepackage__ = __name__
