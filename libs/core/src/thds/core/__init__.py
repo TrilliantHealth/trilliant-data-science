de376a16a7d2cb0c6b52b8629463212362c41fe6
Peter Gaultney <petergaultney@gmail.com>
2023-02-21 15:08:27 -0600

    hardcode thds_core
    GitOrigin-RevId: 50e190f29bdab1de1c7f9e6f5dd19a77a6ef5412

--- libs/core/src/thds/core/__init__.py
+++ libs/core/src/thds/core/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import PackageNotFoundError, version
 from .meta import read_metadata
 
 try:
-    __version__ = version(__name__)
+    __version__ = version("thds_core")
     assert __version__, f"if package {__name__} is found, version should exist."
 except PackageNotFoundError:  # pragma: no cover
     __version__ = ""
