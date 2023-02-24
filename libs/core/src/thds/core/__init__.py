150fda030533ab99024d407424f738ead8d79d10
matt.hawthorn <matt.hawthorn@trillianthealth.com>
2023-02-24 17:13:06 -0500

    merge main
    GitOrigin-RevId: a3174c14b169e0ad7a477fd632ec26a2c0a8d3dd

--- libs/core/src/thds/core/__init__.py
+++ libs/core/src/thds/core/__init__.py
@@ -1,6 +1,8 @@
 """Trilliant Health data science team core utils"""
 from .meta import get_version, read_metadata
 
+# TODO: remove this comment - intended to kick off CI for full monorepo DAG
+
 __version__ = get_version(__name__)
 metadata = read_metadata(__name__)
 __basepackage__ = __name__
