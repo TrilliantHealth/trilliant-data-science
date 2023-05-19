5dfdcdfe691462b47eb938436f0a6bc0af2963a7
Peter Gaultney <petergaultney@gmail.com>
2023-05-18 21:41:26 -0500

    get nppes sqlite generation basically working, with lots of code moved around to support
    GitOrigin-RevId: 7a1330b763bf3685a6c4a21b1b481714b01aa138

---
+++ libs/core/tests/test_core/test_lazy.py
@@ -0,0 +1,36 @@
+from concurrent.futures import ThreadPoolExecutor
+
+from thds.core.lazy import Lazy
+
+
+def test_lazy():
+    counter = 0
+
+    def up():
+        nonlocal counter
+        counter += 1
+        return counter
+
+    lazy_counter = Lazy(up)
+
+    assert lazy_counter() == 1
+    assert counter == 1
+    assert lazy_counter() == 1
+    assert counter == 1
+
+
+def test_lazy_is_threadsafe():
+    counter = 0
+
+    def up():
+        nonlocal counter
+        counter += 1
+        return counter
+
+    lazy_counter = Lazy(up)
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futs = [executor.submit(lazy_counter) for i in range(10)]
+    for fut in futs:
+        assert fut.result() == 1
+    assert counter == 1
