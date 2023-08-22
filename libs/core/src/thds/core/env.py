037227fcb85a8d2c43900a2dc9c6ec76a0aeaf5c
Peter Gaultney <petergaultney@gmail.com>
2023-08-22 13:00:04 -0500

    Merge pull request #507 from TrilliantHealth/task/core/env-context
    Core: add a THDS ENV context var for edge case usage
    GitOrigin-RevId: 756b681aeb41c71c1f080b5df67e7baeec9e0b0c

--- libs/core/src/thds/core/env.py
+++ libs/core/src/thds/core/env.py
@@ -1,20 +1,40 @@
 import os
 import typing as ty
+from contextlib import contextmanager
+
+from .stack_context import StackContext
 
 THDS_ENV = "THDS_ENV"
 
 Env = ty.Literal["", "prod", "dev"]
+
+_TEMP_ENV: StackContext[Env] = StackContext("thds-env", "")
 
 
 def set_active_env(env: Env):
     os.environ[THDS_ENV] = env
 
 
-def get_env() -> Env:
-    """Get the actual value of `THDS_ENV`. Unset == ''."""
+def get_raw_env() -> Env:
+    """Get the actual value of `THDS_ENV`. Unset == ''
+
+    Prefer `active_env` for determining the active environment.
+    """
     return ty.cast(Env, os.environ.get(THDS_ENV, ""))
 
 
+@contextmanager
+def temp_env(env: Env) -> ty.Iterator[Env]:
+    """Temporarily set the value of `THDS_ENV` for the current stack/thread.
+
+    Useful if you have special cases where you need to fetch a result
+    from a different environment than the one you're intending to run,
+    without affecting the global state of your application.
+    """
+    with _TEMP_ENV.set(env):
+        yield _TEMP_ENV()
+
+
 def active_env(override: Env = "") -> Env:
-    """Set or get the effective value of `THDS_ENV`."""
-    return override or get_env() or "dev"
+    """Get the effective value of `THDS_ENV`."""
+    return override or _TEMP_ENV() or get_raw_env() or "dev"
