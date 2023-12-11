9ec9d6e4d10b31bfdd44fddc609ba0c08e40fa5b
Peter Gaultney <petergaultney@gmail.com>
2023-12-11 15:45:24 -0600

    adls uri utilities import cleanup
    GitOrigin-RevId: 034f3734a76b02fad902e6411537dc7a17a90159

--- libs/adls/src/thds/adls/uri.py
+++ libs/adls/src/thds/adls/uri.py
@@ -1,28 +1,24 @@
 import typing as ty
 
-from .abfss import ABFSS_SCHEME
-from .abfss import to_adls_fqn as abfss_to_fqn
-from .dbfs import DBFS_SCHEME
-from .dbfs import to_adls_fqn as dbfs_to_fqn
-from .fqn import ADLS_SCHEME, AdlsFqn, parse
+from . import abfss, dbfs, fqn
 
 HTTPS_SCHEME = "https"
 
 
-def resolve_uri(uri: str) -> ty.Optional[AdlsFqn]:
-    if uri.startswith(ADLS_SCHEME):
-        return parse(uri)
-    elif uri.startswith(ABFSS_SCHEME):
-        return abfss_to_fqn(uri)
+def resolve_uri(uri: str) -> ty.Optional[fqn.AdlsFqn]:
+    if uri.startswith(fqn.ADLS_SCHEME):
+        return fqn.parse(uri)
+    elif uri.startswith(abfss.ABFSS_SCHEME):
+        return abfss.to_adls_fqn(uri)
     elif uri.startswith(HTTPS_SCHEME):
         uri = uri.replace("https://", "adls://")
         uri = uri.replace(".blob.core.windows.net", ".dfs.core.windows.net")
         uri = uri.replace(".dfs.core.windows.net", "")
-        return parse(uri)
-    elif uri.startswith(DBFS_SCHEME):
-        return dbfs_to_fqn(uri)
+        return fqn.parse(uri)
+    elif uri.startswith(dbfs.DBFS_SCHEME):
+        return dbfs.to_adls_fqn(uri)
     return None
 
 
-def resolve_any(fqn_or_uri: ty.Union[str, AdlsFqn]) -> ty.Optional[AdlsFqn]:
+def resolve_any(fqn_or_uri: ty.Union[str, fqn.AdlsFqn]) -> ty.Optional[fqn.AdlsFqn]:
     return resolve_uri(fqn_or_uri) if isinstance(fqn_or_uri, str) else fqn_or_uri
