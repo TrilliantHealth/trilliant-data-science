df2a83cdc58db4164c1ba1b44d94aa0790c8eaa9
Dimitrios Boursinos <dimitrios.boursinos@trillianthealth.com>
2024-04-25 00:57:58 -0500

    Merge pull request #1008 from TrilliantHealth/bugfix/malformed-docstring
    Bugfix/malformed docstring
    
    GitOrigin-RevId: 320272cf54100d39b71cfa565d2e95f2271b4c6f

--- libs/attrs-utils/tests/attrs_utils/test_docs.py
+++ libs/attrs-utils/tests/attrs_utils/test_docs.py
@@ -1,7 +1,12 @@
-from thds.attrs_utils.docs import record_class_docs
+from dataclasses import dataclass
+
+import pytest
+
+from thds.attrs_utils.docs import get_record_class_fields, record_class_docs
 
 
-class Record1:
+@dataclass
+class Record1:  # noqa
     """Record1 doc
 
     :param a: Record1 a
@@ -11,8 +16,13 @@ class Record1:
     :return str: Record1 return
     """
 
+    a: int
+    b: str
+    c: float
 
-class Record2(Record1):
+
+@dataclass
+class Record2(Record1):  # noqa
     """Record2 doc
 
     Record2 long desc
@@ -23,8 +33,13 @@ class Record2(Record1):
     :return int: Record2 return
     """
 
+    b: str
+    c: float
+    d: bool
 
-class Record3(Record2):
+
+@dataclass
+class Record3(Record2):  # noqa
     """Record3 doc
 
 
@@ -38,33 +53,108 @@ class Record3(Record2):
     :raises RuntimeError: Record3 raises 2
     """
 
+    a: int
+    d: bool
+    e: str
 
-def test_record_class_docs():
-    docs = record_class_docs(Record3)
 
-    expected_params = dict(
-        a="Record3 a",
-        b="Record2 b",
-        c="Record2 c",
-        d="Record3 d",
-        e="Record3 e",
-    )
-    expected_return = "Record2 return"
-    expected_return_type = "int"
-    expected_raises = [("ValueError", "Record3 raises 1"), ("RuntimeError", "Record3 raises 2")]
-    expected_short_desc = "Record3 doc"
-    expected_long_desc = "Record3 long desc"
+# param e missing docstring
+@dataclass
+class Record4(Record2):
+    """Record4 doc
 
-    assert {p.arg_name: p.description for p in docs.params} == expected_params
-    assert docs.returns is not None
-    assert docs.returns.description == expected_return
-    assert docs.returns.type_name == expected_return_type
-    assert [(r.type_name, r.description) for r in docs.raises] == expected_raises
-    assert docs.short_description == expected_short_desc
-    assert docs.long_description == expected_long_desc
+    Record4 long desc
 
-    expected_short_desc = "Record1 doc\nRecord2 doc\nRecord3 doc"
-    expected_long_desc = "Record2 long desc\nRecord3 long desc"
-    docs = record_class_docs(Record3, combine_docs="join", join_sep="\n")
-    assert docs.short_description == expected_short_desc
-    assert docs.long_description == expected_long_desc
+    :param a: Record4 a
+    :param d: Record4 d
+    :raises ValueError: Record4 raises 1
+    :raises RuntimeError: Record4 raises 2
+    """
+
+    a: int
+    d: bool
+    e: str
+
+
+@pytest.mark.parametrize(
+    "c, expected_params, expected_return, expected_return_type, expected_raises, expected_short_desc, expected_long_desc, expected_value_error",
+    [
+        pytest.param(
+            Record3,
+            {
+                "a": "Record3 a",
+                "b": "Record2 b",
+                "c": "Record2 c",
+                "d": "Record3 d",
+                "e": "Record3 e",
+            },
+            "Record2 return",
+            "int",
+            [("ValueError", "Record3 raises 1"), ("RuntimeError", "Record3 raises 2")],
+            "Record3 doc",
+            "Record3 long desc",
+            None,
+            id="Record3",
+        ),
+        pytest.param(
+            Record4,
+            {
+                "a": "Record4 a",
+                "b": "Record2 b",
+                "c": "Record2 c",
+                "d": "Record2 d",
+            },
+            "Record2 return",
+            "int",
+            [("ValueError", "Record4 raises 1"), ("RuntimeError", "Record4 raises 2")],
+            "Record4 doc",
+            "Record4 long desc",
+            "Missing docstring params for Record4: {'e'}",
+            id="Record4",
+        ),
+    ],
+)
+def test_record_class_docs(
+    c,
+    expected_params,
+    expected_return,
+    expected_return_type,
+    expected_raises,
+    expected_short_desc,
+    expected_long_desc,
+    expected_value_error,
+):
+    if expected_value_error:
+        with pytest.raises(ValueError) as e:
+            record_class_docs(c, require_complete=True)
+        assert str(e.value) == expected_value_error
+        return
+    else:
+        docs = record_class_docs(c, require_complete=True)
+
+        assert {p.arg_name: p.description for p in docs.params} == expected_params
+        assert docs.returns is not None
+        assert docs.returns.description == expected_return
+        assert docs.returns.type_name == expected_return_type
+        assert [(r.type_name, r.description) for r in docs.raises] == expected_raises
+        assert docs.short_description == expected_short_desc
+        assert docs.long_description == expected_long_desc
+
+        expected_short_desc = "Record1 doc\nRecord2 doc\nRecord3 doc"
+        expected_long_desc = "Record2 long desc\nRecord3 long desc"
+        docs = record_class_docs(Record3, combine_docs="join", join_sep="\n")
+        assert docs.short_description == expected_short_desc
+        assert docs.long_description == expected_long_desc
+
+
+@pytest.mark.parametrize(
+    "c,expected",
+    [
+        pytest.param(Record1, {"a", "b", "c"}, id="Record1"),
+        pytest.param(Record2, {"a", "b", "c", "d"}, id="Record2"),
+        pytest.param(Record3, {"a", "b", "c", "d", "e"}, id="Record3"),
+    ],
+)
+def test_get_record_class_fields(c, expected):
+    attrs = get_record_class_fields(c)
+    assert attrs == expected
