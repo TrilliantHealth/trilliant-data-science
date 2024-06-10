c74924234f2b44ed712d486ca4356bada86facdc
Rueben Banalagay <rueben.banalagay@trillianthealth.com>
2024-06-10 11:46:04 -0500

    Merge branch 'feature/unified-directory/provider-specialization' into refactor/provider-specialization/materialize_features
    GitOrigin-RevId: f4f947cc864570796d074e46d39881ee8ec73fd5

--- libs/core/src/thds/core/hash_cache.py
+++ libs/core/src/thds/core/hash_cache.py
@@ -15,16 +15,13 @@ import os
 from pathlib import Path
 from typing import Any
 
-from . import config
+from .config import ConfigItem
 from .hashing import Hash, hash_using
 from .home import HOMEDIR
 from .log import getLogger
 from .types import StrOrPath
 
-CACHE_HASH_DIR = config.item("directory", HOMEDIR() / ".hash-cache", parse=Path)
-_1GB = 1 * 2**30  # log if hashing a file larger than this, since it will be slow.
-
-
+CACHE_HASH_DIR = ConfigItem("directory", HOMEDIR() / ".hash-cache", parse=Path)
 logger = getLogger(__name__)
 
 
@@ -63,10 +60,6 @@ def hash_file(filepath: StrOrPath, hasher: Any) -> bytes:
     ):
         logger.debug("Reusing known hash %s", resolved_path)
         return cached_hash_location.read_bytes()
-
-    psize = Path(resolved_path).stat().st_size
-    if psize > _1GB:
-        logger.info(f"Hashing downloaded {psize/_1GB:.2f} GB file at {resolved_path}...")
 
     hash_bytes = hash_using(resolved_path, hasher).digest()
     cached_hash_location.parent.mkdir(parents=True, exist_ok=True)
