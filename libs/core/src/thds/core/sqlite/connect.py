9d69b52018e6b142df78f5d041b82f492b0f1668
cheick.berthe <cheick.berthe@trillianthealth.com>
2024-06-14 01:11:44 -0400

    move sqlite and update paths
    GitOrigin-RevId: d4655fd6c78ed970c215829a0f4e74dfe812e59b

---
+++ libs/core/src/thds/core/sqlite/connect.py
@@ -0,0 +1,27 @@
+import os
+import sqlite3
+import typing as ty
+from pathlib import Path
+
+from thds.core import scope
+
+from .functions import register_functions
+from .types import Connectable
+
+
+def row_connect(path: ty.Union[str, os.PathLike]) -> sqlite3.Connection:
+    """Get a connection to a row database"""
+    conn = sqlite3.connect(os.fspath(path))
+    conn.row_factory = sqlite3.Row
+    register_functions(conn)
+    return conn
+
+
+autoconn_scope = scope.Scope("sqlite3.autoconn")
+
+
+def autoconnect(connectable: Connectable) -> sqlite3.Connection:
+    if isinstance(connectable, (str, Path)):
+        return autoconn_scope.enter(sqlite3.connect(str(connectable)))
+    assert isinstance(connectable, sqlite3.Connection)
+    return connectable
