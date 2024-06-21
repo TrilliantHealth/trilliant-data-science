b475e19ed3df0314267605767ac380024a9b6f9d
Peter Gaultney <petergaultney@gmail.com>
2024-06-21 14:21:59 -0400

    set up dbxtend main that calls into mops main
    GitOrigin-RevId: 4c0a768bf27975eb10fe48e96d190828be3bd871

--- libs/mops/src/thds/mops/__init__.py
+++ libs/mops/src/thds/mops/__init__.py
@@ -1,9 +1,2 @@
 from .__about__ import __commit__, __version__  # noqa
 from ._utils.temp import tempdir  # noqa
-
-
-def main():
-    """this exists purely because of DBX and for no other reason."""
-    from thds.mops.pure.core.entry.main import main
-
-    main()
