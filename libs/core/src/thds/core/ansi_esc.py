4f455503926170598dbe974313136cbbfd909663
Peter Gaultney <petergaultney@gmail.com>
2024-08-29 14:06:26 -0400

    fix small bug in how i was resetting ansi styles; improve module name; add bright red and blink :)
    GitOrigin-RevId: 07c1ce485db7908c3532cb19939fc083e40bb72e

--- libs/core/src/thds/core/ansi_esc.py
+++ libs/core/src/thds/core/ansi_esc.py
@@ -1,4 +1,5 @@
 # thanks to https://gist.github.com/minism/1590432
+# and https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 
 
 class fg:
@@ -10,7 +11,10 @@ class fg:
     MAGENTA = "\033[35m"
     CYAN = "\033[36m"
     WHITE = "\033[37m"
-    RESET = "\033[39m"
+
+    ERROR_RED = "\033[38;5;196m"
+
+    RESET = "\033[39m"  # a.k.a. DEFAULT
 
 
 class bg:
@@ -22,11 +26,21 @@ class bg:
     MAGENTA = "\033[45m"
     CYAN = "\033[46m"
     WHITE = "\033[47m"
-    RESET = "\033[49m"
+
+    ERROR_RED = "\033[48;5;196m"
+
+    RESET = "\033[49m"  # a.k.a. DEFAULT
 
 
 class style:
     BRIGHT = "\033[1m"
     DIM = "\033[2m"
     NORMAL = "\033[22m"
+
+    BLINK = "\033[5m"
+    NO_BLINK = "\033[25m"
+
+    ITALIC = "\033[3m"
+    NO_ITALIC = "\033[23m"
+
     RESET_ALL = "\033[0m"
