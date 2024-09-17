d07bab769b8f871dc941333fb0da34f4c6850304
Peter Gaultney <petergaultney@gmail.com>
2024-09-17 18:33:47 -0400

    Merge branch 'main' into feature/core/json-logs
    GitOrigin-RevId: 3b33beb8b98b3104c3ed4dd80c1668538f6db4a5

---
+++ libs/core/tests/test_core/test_log_json.py
@@ -0,0 +1,25 @@
+import logging
+
+from thds.core.log.json_formatter import ThdsJsonFormatter
+
+
+def test_json_format(caplog):
+
+    formatter = ThdsJsonFormatter()
+
+    record = logging.LogRecord(
+        "name",
+        logging.INFO,
+        "tests/test_core/test_log_json.py",
+        1,
+        "This is a test message",
+        (),
+        None,
+    )
+    record.th_context = dict(foo="bar", baz="qux")
+
+    fmtted_msg = formatter.format(record)
+    print(fmtted_msg)
+    assert fmtted_msg.endswith(
+        '"level": "INFO", "module": "test_log_json", "msg": "This is a test message", "foo": "bar", "baz": "qux"}'
+    )
