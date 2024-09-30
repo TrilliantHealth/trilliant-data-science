f8fa1cd5650f0d91f220ae71fe883b3e70474826
Peter Gaultney <petergaultney@gmail.com>
2024-09-30 10:37:45 -0400

    add resolve_lazy_db_and_table helper to core.sqlite
    GitOrigin-RevId: 58013566a4e619578ef95628d34d0edc14c7a568

--- libs/core/src/thds/core/sqlite/__init__.py
+++ libs/core/src/thds/core/sqlite/__init__.py
@@ -9,5 +9,13 @@ from .meta import (  # noqa: F401
     table_source,
 )
 from .structured import StructTable, struct_table_from_source  # noqa: F401
-from .types import DbAndTable, DbAndTableP, TableMaster, TableSource, maybe_t  # noqa: F401
+from .types import (  # noqa: F401
+    AnyDbTableSrc,
+    DbAndTable,
+    DbAndTableP,
+    TableMaster,
+    TableSource,
+    maybe_t,
+    resolve_lazy_db_and_table,
+)
 from .write import make_mapping_writer, write_mappings  # noqa: F401
