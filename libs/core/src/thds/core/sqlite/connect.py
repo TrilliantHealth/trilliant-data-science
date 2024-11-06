8514b0a489cbca66e60af6526c5597dd7ea65316
Peter Gaultney <petergaultney@gmail.com>
2024-11-05 23:12:24 -0500

    slightly adjust core.sqlite.autoconnect semantics and organization
    GitOrigin-RevId: ebe74e4842e627b8e94af894b54a7476ff6c129b

--- libs/core/src/thds/core/sqlite/connect.py
+++ libs/core/src/thds/core/sqlite/connect.py
@@ -11,27 +11,23 @@ from .types import Connectable
 
 def row_connect(path: ty.Union[str, os.PathLike]) -> sqlite3.Connection:
     """Get a connection to a row database"""
-    conn = sqlite3.connect(os.fspath(path))
+    conn = sqlite3.connect(os.fspath(path), isolation_level=None)  # autocommit
     conn.row_factory = sqlite3.Row
-    register_functions_on_connection(conn)
-    return conn
+    return register_functions_on_connection(conn)
 
 
 autoconn_scope = scope.Scope("sqlite3.autoconn")
 
 
 def autoconnect(connectable: Connectable) -> sqlite3.Connection:
-    """Will automatically commit when it hits the autoconn_scope.bound"""
+    """Will automatically commit when it hits the autoconn_scope.bound, but only if
+    the connectable was not already a connection.
+    """
     if isinstance(connectable, sqlite3.Connection):
         return connectable
 
     return autoconn_scope.enter(
         contextlib.closing(  # close the connection when we exit the scope
-            register_functions_on_connection(
-                sqlite3.connect(
-                    os.fspath(connectable),
-                    isolation_level=None,  # autocommit
-                )
-            )
+            row_connect(os.fspath(connectable))
         )
     )
