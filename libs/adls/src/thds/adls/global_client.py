69b01caae8641c7561ad4bee5b43ce293fcb77ff
Peter Gaultney <petergaultney@gmail.com>
2024-11-21 22:37:39 +0000

    Merge pull request #2059 from TrilliantHealth/feature/adls/control-urllib3-connpool-size
    ADLS: control urllib3 connpool size
    GitOrigin-RevId: 019adc29a4abb202071975d42697617a1186781f

--- libs/adls/src/thds/adls/global_client.py
+++ libs/adls/src/thds/adls/global_client.py
@@ -1,17 +1,46 @@
+import requests
 from azure.storage.blob import BlobServiceClient, ContainerClient
 from azure.storage.filedatalake import DataLakeServiceClient, FileSystemClient
 
-from thds.core import cache
+from thds.core import cache, config
 
 from . import conf
 from .shared_credential import SharedCredential
 
+DEFAULT_CONNECTION_POOL_SIZE = config.item("default_connection_pool_size", default=100, parse=int)
+# see docstring below about why we are setting this to be large.
 
-def adls_fs_client(storage_account: str, container: str) -> FileSystemClient:
+
+def _connpool_session(connection_pool_size: int) -> requests.Session:
+    """We do lot of multithreaded use of connections to ADLS. For large runs,
+    having the default connection pool size of 10 is not enough, because each thread will create a
+    new connection if an unused one is not available from the pool, and we often have more than 10 threads
+    communicating at a time.
+
+    The connection pool will not start out this large, so don't fear that we're introducing new overhead here.
+    We will just throw away fewer connections that were used only once and replaced by a newer one.
+
+    The only reason you'd really want to keep this low is if the host you're talking to
+    can't handle this many simultaneous connections, but my belief is that ADLS should be
+    able to handle thousands (probably hundreds of thousands?) of connections at once.
+    """
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(
+        pool_connections=connection_pool_size, pool_maxsize=connection_pool_size
+    )
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
+    return session
+
+
+def adls_fs_client(
+    storage_account: str, container: str, connpool_size: int = DEFAULT_CONNECTION_POOL_SIZE()
+) -> FileSystemClient:
     """No context managers - is this better?"""
     return DataLakeServiceClient(
         account_url=f"https://{storage_account}.dfs.core.windows.net",
         credential=SharedCredential(),
+        session=_connpool_session(connection_pool_size=connpool_size),
         max_single_get_size=conf.MAX_SINGLE_GET_SIZE(),  # for downloads
         max_chunk_get_size=conf.MAX_CHUNK_GET_SIZE(),  # for downloads
         max_block_size=conf.MAX_BLOCK_SIZE(),  # for uploads
@@ -29,13 +58,16 @@ get_global_client = cache.locking(adls_fs_client)
 get_global_fs_client = get_global_client
 
 
-def adls_blob_container_client(storage_account: str, container: str) -> ContainerClient:
+def adls_blob_container_client(
+    storage_account: str, container: str, connpool_size: int = DEFAULT_CONNECTION_POOL_SIZE()
+) -> ContainerClient:
     """This seems to support an atomic write operation,
     which is in theory going to be faster than two separate network requests.
     """
     return BlobServiceClient(
         account_url=f"https://{storage_account}.blob.core.windows.net",
         credential=SharedCredential(),
+        session=_connpool_session(connection_pool_size=connpool_size),
         max_single_get_size=conf.MAX_SINGLE_GET_SIZE(),  # for downloads
         max_chunk_get_size=conf.MAX_CHUNK_GET_SIZE(),  # for downloads
         max_block_size=conf.MAX_BLOCK_SIZE(),  # for uploads
