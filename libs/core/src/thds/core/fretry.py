ec669c53c6df0a92dbb2889827c6cb104d959ed9
Peter Gaultney <petergaultney@gmail.com>
2025-01-03 12:07:05 -0500

    improve retry semantics in a few places
    improve formatting
    
    switch to warnings for retries in kvdb
    
    fix some retry issues
    
    more retry fixups
    
    GitOrigin-RevId: 43dc39014e59cab0a191173272701ea89baca74e

--- libs/core/src/thds/core/fretry.py
+++ libs/core/src/thds/core/fretry.py
@@ -1,9 +1,11 @@
 """A more composable retry decorator."""
+
 import random
 import time
 import typing as ty
 from functools import wraps
 from logging import getLogger
+from timeit import default_timer
 
 F = ty.TypeVar("F", bound=ty.Callable)
 
@@ -18,16 +20,24 @@ def expo(
 ) -> ty.Callable[[], ty.Iterator[float]]:
     """End iteration after yielding 'retries' times.
 
+    The first retry is immediate (i.e. 0). Subsequent retries will follow the schedule
+    established by the exponential backoff algorithm. The default algorithm is 1, 3, 7,
+    15, etc., but also adds jitter.
+
     If you want infinite exponential values, pass a negative number for 'retries'.
     """
 
     def expo_() -> ty.Iterator[float]:
         count = 0
+        accum_jitter = 0.0
         while retries < 0 or count < retries:
-            expo_delay = backoff**count * delay
+            expo_delay = (backoff**count * delay) - delay  # first retry is immediate
             if jitter:
-                expo_delay *= random.uniform(0.5, 1.5)
-            yield expo_delay
+                jitter_delay = random.uniform(0.5, 1.5) * expo_delay
+                yield jitter_delay + accum_jitter
+                accum_jitter = expo_delay - jitter_delay
+            else:
+                yield expo_delay
             count += 1
 
     return expo_
@@ -36,12 +46,20 @@ def expo(
 def sleep(
     mk_seconds_iter: ty.Callable[[], ty.Iterable[float]],
     sleeper: ty.Callable[[float], ty.Any] = time.sleep,
-) -> ty.Callable[[], ty.Iterator]:
-    """A common base strategy for separating retries by sleeps."""
+) -> ty.Callable[[], ty.Iterator[str]]:
+    """A common base strategy for separating retries by sleeps.
 
-    def sleep_() -> ty.Iterator:
-        for secs in mk_seconds_iter():
-            yield
+    Yield once prior to the first sleep, and once before each sleep.
+    In other words, the total number of yields is the length of the input iterable (if it is finite).
+    """
+
+    def sleep_() -> ty.Iterator[str]:
+        start = default_timer()
+
+        so_far = 0.0
+        for i, secs in enumerate(mk_seconds_iter(), start=1):
+            yield f"attempt {i} after {so_far:.2f}s"
+            so_far = default_timer() - start
             sleeper(secs)
 
     return sleep_
@@ -60,13 +78,14 @@ def retry(retry_strategy_factory: RetryStrategyFactory) -> ty.Callable[[F], F]:
     def _retry_decorator(func: F) -> F:
         @wraps(func)
         def retry_wrapper(*args, **kwargs):
-            for is_retryable in retry_strategy_factory():
+            for i, is_retryable in enumerate(retry_strategy_factory(), start=1):
                 try:
                     return func(*args, **kwargs)
                 except Exception as ex:
                     if not is_retryable(ex):
                         raise ex
-                    getLogger(__name__).info(f"Retrying {func} due to {ex}")
+                    getLogger(__name__).info("Retry #%d for %s due to exception %s", i, func, ex)
+            # one final retry that, if it fails, will not get caught and retried.
             return func(*args, **kwargs)
 
         return ty.cast(F, retry_wrapper)
@@ -85,7 +104,7 @@ def retry_sleep(
     is_retryable: IsRetryable,
     seconds_iter: ty.Callable[[], ty.Iterable[float]],
 ) -> ty.Callable[[F], F]:
-    """E.g. retry_sleep(expo(retries=5))"""
+    """E.g. retry_sleep(expo(retries=5)) to get max 6 calls to the function."""
     return retry_regular(is_retryable, sleep(seconds_iter))
 
 
