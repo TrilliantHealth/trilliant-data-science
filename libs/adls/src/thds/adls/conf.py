ede08aa3fb5849cc86c21ab1e728b4d78891ca70
Grant Andersen <gandersen.codes@gmail.com>
2025-01-17 13:34:48 -0600

    refactor of 'copy.copy_file'
    GitOrigin-RevId: 5f1ead74eed49ec3977dbde996eeb2b0f44e6d00

--- libs/adls/src/thds/adls/conf.py
+++ libs/adls/src/thds/adls/conf.py
@@ -31,3 +31,9 @@ UPLOAD_FILE_MAX_CONCURRENCY = config.item("upload_file_max_concurrency", 10, par
 UPLOAD_CHUNK_SIZE = config.item("upload_chunk_size", 2**20 * 100, parse=int)  # 100 MB
 
 CONNECTION_TIMEOUT = config.item("connection_timeout", 2000, parse=int)  # seconds
+
+# these are for SAS tokens
+# 15 minutes seems like a decent default for the time it would take to complete a SAS requiring operation
+USER_DELEGATION_KEY_EXPIRY = config.ConfigItem("users_delegation_key_expiry", 900, parse=int)  # seconds
+# this key needs to remain valid for signed SAS tokens to remain valid
+BLOB_SAS_EXPIRY = config.ConfigItem("blob_sas_expiry", 900, parse=int)  # seconds
--- libs/adls/src/thds/adls/copy.py
+++ libs/adls/src/thds/adls/copy.py
@@ -1,26 +1,25 @@
-"""Functions for making copying blobs across remote locations."""
+"""Functions for copying blobs across remote locations."""
 import datetime
 import random
 import time
 import typing as ty
 
-from azure.storage.blob import BlobSasPermissions, generate_blob_sas
+from azure.storage.blob import BlobSasPermissions, BlobServiceClient, UserDelegationKey
 
-from thds.core.log import getLogger
+from thds.core import log, thunks
 
 from .file_properties import exists, get_blob_properties, get_file_properties, is_directory
 from .fqn import AdlsFqn
 from .global_client import get_global_blob_container_client, get_global_blob_service_client
+from .sas_tokens import gen_blob_sas_token, get_user_delegation_key
 from .uri import UriIsh, parse_any
 
-logger = getLogger(__name__)
-
-_AUTH_EXPIRY_MINS = 15
-# seems like copying a single file shouldn't take longer than 15 minutes
-# not sure if the copy would fail if auth expires during the copy either
+logger = log.getLogger(__name__)
 
 
-OverwriteMethod = ty.Literal["error", "warn", "skip"]
+OverwriteMethod = ty.Literal["error", "warn", "skip", "silent"]
+
+CopyProperties = ty.Dict[str, ty.Union[str, datetime.datetime]]
 
 
 def _copy_file(
@@ -28,65 +27,62 @@ def _copy_file(
     dest: AdlsFqn,
     *,
     overwrite_method: OverwriteMethod = "error",
-) -> AdlsFqn:
+    get_account_key: ty.Callable[
+        [BlobServiceClient], ty.Union[str, UserDelegationKey]
+    ] = get_user_delegation_key,
+) -> CopyProperties:
     if is_directory(get_file_properties(src)):
-        raise ValueError(f"{src!r} is a directory.")
+        raise ValueError(f"{src} is a directory.")
 
     if exists(dest):
         if overwrite_method == "error":
-            raise ValueError(f"'{dest}' already exists!")
+            raise ValueError(f"{dest} already exists!")
         elif overwrite_method == "warn":
-            logger.warning("'%s' already exists and will be overwritten with '%s'", dest, src)
+            logger.warning("%s already exists and will be overwritten with %s", dest, src)
+        elif overwrite_method == "skip":
+            logger.warning("%s already exists, no copy from %s will occur", dest, src)
+            return dict()
         else:
-            logger.warning("'%s' already exists, no copy from '%s' will occur", dest, src)
-            return dest
+            return dict()
 
-    src_bsc = get_global_blob_service_client(src.sa)
-    now = datetime.datetime.now(datetime.timezone.utc)
-
-    sas_token = generate_blob_sas(
-        src.sa,
-        src.container,
-        src.path,
-        account_key=src_bsc.get_user_delegation_key(  # type: ignore[arg-type]
-            key_start_time=now, key_expiry_time=now + datetime.timedelta(minutes=_AUTH_EXPIRY_MINS)
-        ),  # does this key need to live for the life of the token(s) it signs?
-        # on the other hand could also try to cache this key to prevent too frequent requests for a key
-        permission=BlobSasPermissions(read=True),
-        expiry=datetime.datetime.now(datetime.timezone.utc)
-        + datetime.timedelta(minutes=_AUTH_EXPIRY_MINS),
-    )
-    # TODO - make this a generic function
-
-    src_blob_client = src_bsc.get_blob_client(src.container, src.path)
+    src_blob_client = get_global_blob_container_client(src.sa, src.container).get_blob_client(src.path)
     dest_blob_client = get_global_blob_container_client(dest.sa, dest.container).get_blob_client(
         dest.path
     )
 
-    logger.info("Copying '%s' to '%s'...", src, dest)
-    dest_blob_client.start_copy_from_url(f"{src_blob_client.url}?{sas_token}")
+    sas_token = gen_blob_sas_token(
+        src,
+        account_key=get_account_key(get_global_blob_service_client(src.sa)),
+        permissions=BlobSasPermissions(read=True),
+    )
 
-    return dest
+    logger.info("Copying %s to %s...", src, dest)
+    return dest_blob_client.start_copy_from_url(f"{src_blob_client.url}?{sas_token}")
 
 
 def wait_for_copy(fqn: AdlsFqn, timeout: int = 300) -> AdlsFqn:
+    """Set timeout to 0 or a negative int to wait indefinitely."""
     start_time = time.time()
 
-    while time.time() - start_time < timeout:
-        copy_props = get_blob_properties(fqn).copy
-        if copy_props.status == "success":
-            # blob properties actually shows copy progress so we could add a progress bar
+    while (time.time() - start_time < timeout) if timeout > 0 else True:
+        blob_copy_props = get_blob_properties(fqn).copy
+        # blob copy properties actually show copy progress so we could add a progress bar
+
+        if blob_copy_props.status == "success":
             return fqn
-        elif copy_props.status is None:
-            raise ValueError(f"A copy to '{fqn}', is not currently in progress.")
-        elif copy_props.status != "pending":
+        elif blob_copy_props.status is None:
+            raise ValueError(f"{fqn} is not an a pending or completed copy.")
+        elif blob_copy_props.status != "pending":
             raise ValueError(
-                f"The copy to '{fqn}' {copy_props.status}. See copy properties: {copy_props}"
+                f"The copy to {fqn} failed with the status: {blob_copy_props.status}. "
+                f"See blob copy properties: {blob_copy_props}"
             )
 
         time.sleep(random.randint(1, 5))
 
-    raise TimeoutError(f"Copying to '{fqn}' did not finish within {timeout} seconds.")
+    raise TimeoutError(
+        f"Copying to {fqn} did not finish within {timeout} seconds. It may still be copying."
+    )
 
 
 def copy_file(
@@ -96,15 +92,21 @@ def copy_file(
     overwrite_method: OverwriteMethod = "error",
     wait: bool = True,
     timeout: int = 300,
-) -> AdlsFqn:
+    get_account_key: ty.Callable[
+        [BlobServiceClient], ty.Union[str, UserDelegationKey]
+    ] = get_user_delegation_key,
+) -> CopyProperties:
     src_fqn = parse_any(src)
     dest_fqn = parse_any(dest)
 
-    if wait:
-        copy = wait_for_copy(
-            _copy_file(src_fqn, dest_fqn, overwrite_method=overwrite_method), timeout=timeout
-        )
-        logger.info("Finished copying '%s' to '%s'", src_fqn, dest_fqn)
-        return copy
+    func = thunks.thunking(_copy_file)(
+        src_fqn, dest_fqn, overwrite_method=overwrite_method, get_account_key=get_account_key
+    )
 
-    return _copy_file(src_fqn, dest_fqn, overwrite_method=overwrite_method)
+    if wait:
+        copy_props = func()
+        wait_for_copy(dest_fqn, timeout=timeout)
+        logger.info("Finished copying %s to %s", src_fqn, dest_fqn)
+        return copy_props
+
+    return func()
