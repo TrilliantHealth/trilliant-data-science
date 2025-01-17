53262d8a76b32421177480f6b2e007a8db2404ae
Grant Andersen <gandersen.codes@gmail.com>
2025-01-17 16:02:39 -0600

    implementing 'copy.copy_files'
    GitOrigin-RevId: 7206c9c20dad350f98a6a690cf5998bab6452c28

--- libs/adls/src/thds/adls/copy.py
+++ libs/adls/src/thds/adls/copy.py
@@ -6,8 +6,9 @@ import typing as ty
 
 from azure.storage.blob import BlobSasPermissions, BlobServiceClient, UserDelegationKey
 
-from thds.core import log, thunks
+from thds.core import cache, log, parallel, thunks
 
+from .conf import BLOB_SAS_EXPIRY
 from .file_properties import exists, get_blob_properties, get_file_properties, is_directory
 from .fqn import AdlsFqn
 from .global_client import get_global_blob_container_client, get_global_blob_service_client
@@ -16,21 +17,20 @@ from .uri import UriIsh, parse_any
 
 logger = log.getLogger(__name__)
 
+_COPY_TIMEOUT = BLOB_SAS_EXPIRY()  # seconds
+
 
 OverwriteMethod = ty.Literal["error", "warn", "skip", "silent"]
 
-CopyProperties = ty.Dict[str, ty.Union[str, datetime.datetime]]
+CopyRequest = ty.Dict[str, ty.Union[str, datetime.datetime]]
 
 
 def _copy_file(
     src: AdlsFqn,
     dest: AdlsFqn,
-    *,
+    account_key: ty.Union[str, UserDelegationKey],
     overwrite_method: OverwriteMethod = "error",
-    get_account_key: ty.Callable[
-        [BlobServiceClient], ty.Union[str, UserDelegationKey]
-    ] = get_user_delegation_key,
-) -> CopyProperties:
+) -> CopyRequest:
     if is_directory(get_file_properties(src)):
         raise ValueError(f"{src} is a directory.")
 
@@ -51,10 +51,9 @@ def _copy_file(
     )
 
     sas_token = gen_blob_sas_token(
-        src,
-        account_key=get_account_key(get_global_blob_service_client(src.sa)),
-        permissions=BlobSasPermissions(read=True),
+        src, account_key=account_key, permissions=BlobSasPermissions(read=True)
     )
+    # we may not always need a SAS token for the copy, but it would be hard to reason about when they are(n't)
 
     logger.info("Copying %s to %s...", src, dest)
     return dest_blob_client.start_copy_from_url(f"{src_blob_client.url}?{sas_token}")
@@ -78,7 +77,7 @@ def wait_for_copy(fqn: AdlsFqn, timeout: int = 300) -> AdlsFqn:
                 f"See blob copy properties: {blob_copy_props}"
             )
 
-        time.sleep(random.randint(1, 5))
+        time.sleep(random.uniform(1, 3))
 
     raise TimeoutError(
         f"Copying to {fqn} did not finish within {timeout} seconds. It may still be copying."
@@ -88,19 +87,21 @@ def wait_for_copy(fqn: AdlsFqn, timeout: int = 300) -> AdlsFqn:
 def copy_file(
     src: UriIsh,
     dest: UriIsh,
-    *,
     overwrite_method: OverwriteMethod = "error",
     wait: bool = True,
-    timeout: int = 300,
+    timeout: int = _COPY_TIMEOUT,
     get_account_key: ty.Callable[
         [BlobServiceClient], ty.Union[str, UserDelegationKey]
     ] = get_user_delegation_key,
-) -> CopyProperties:
+) -> CopyRequest:
     src_fqn = parse_any(src)
     dest_fqn = parse_any(dest)
 
     func = thunks.thunking(_copy_file)(
-        src_fqn, dest_fqn, overwrite_method=overwrite_method, get_account_key=get_account_key
+        src_fqn,
+        dest_fqn,
+        account_key=get_account_key(get_global_blob_service_client(src_fqn.sa)),
+        overwrite_method=overwrite_method,
     )
 
     if wait:
@@ -110,3 +111,33 @@ def copy_file(
         return copy_props
 
     return func()
+
+
+def copy_files(
+    srcs_to_dests: ty.Mapping[UriIsh, UriIsh],
+    overwrite_method: OverwriteMethod = "error",
+    wait: bool = True,
+    timeout: int = _COPY_TIMEOUT,
+    get_account_key: ty.Callable[
+        [BlobServiceClient], ty.Union[str, UserDelegationKey]
+    ] = get_user_delegation_key,
+) -> ty.Dict[AdlsFqn, CopyRequest]:
+    src_fqns_to_dest_fqns = {parse_any(k): parse_any(v) for k, v in srcs_to_dests.items()}
+    get_account_key = cache.locking(get_account_key)
+
+    def copy_wrapper(src: AdlsFqn, dest: AdlsFqn) -> ty.Tuple[AdlsFqn, CopyRequest]:
+        return dest, copy_file(
+            src,
+            dest,
+            overwrite_method=overwrite_method,
+            wait=wait,
+            timeout=timeout,
+            get_account_key=get_account_key,
+        )
+
+    # would be cool to do this async, but using threads for quicker dev
+    return dict(
+        parallel.yield_results(
+            [thunks.thunking(copy_wrapper)(src, dest) for src, dest in src_fqns_to_dest_fqns.items()]
+        )
+    )
