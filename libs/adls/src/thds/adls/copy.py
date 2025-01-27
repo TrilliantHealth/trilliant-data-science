915e7c81223840923e480fedc6572d093d9ea8bb
Grant Andersen <gandersen.codes@gmail.com>
2025-01-27 15:25:15 -0600

    refactor 'copy_file' output + 'copy_files' input and output
    GitOrigin-RevId: e481f7d6d425ab5bea5c1e2b0d987b9b0b8f108f

--- libs/adls/src/thds/adls/copy.py
+++ libs/adls/src/thds/adls/copy.py
@@ -8,8 +8,7 @@ from azure.storage.blob import BlobSasPermissions, BlobServiceClient, UserDelega
 
 from thds.core import cache, log, parallel, thunks
 
-from .conf import BLOB_SAS_EXPIRY
-from .file_properties import exists, get_blob_properties, get_file_properties, is_directory
+from .file_properties import get_blob_properties, get_file_properties, is_directory
 from .fqn import AdlsFqn
 from .global_client import get_global_blob_container_client, get_global_blob_service_client
 from .sas_tokens import gen_blob_sas_token, get_user_delegation_key
@@ -17,7 +16,7 @@ from .uri import UriIsh, parse_any
 
 logger = log.getLogger(__name__)
 
-_COPY_TIMEOUT = BLOB_SAS_EXPIRY()  # seconds
+_WAIT_TIMEOUT = 300  # seconds
 
 
 OverwriteMethod = ty.Literal["error", "warn", "skip", "silent"]
@@ -25,30 +24,62 @@ OverwriteMethod = ty.Literal["error", "warn", "skip", "silent"]
 CopyRequest = ty.Dict[str, ty.Union[str, datetime.datetime]]
 
 
+class CopyInfo(ty.NamedTuple):
+    src: AdlsFqn
+    dest: AdlsFqn
+    request: CopyRequest
+
+    @property
+    def copy_occurred(self) -> bool:
+        return bool(self.request)
+
+
 def _copy_file(
     src: AdlsFqn,
     dest: AdlsFqn,
     account_key: ty.Union[str, UserDelegationKey],
     overwrite_method: OverwriteMethod = "error",
-) -> CopyRequest:
-    if is_directory(get_file_properties(src)):
-        raise ValueError(f"{src} is a directory.")
+) -> CopyInfo:
+    if not (file_properties := get_file_properties(src)):
+        raise ValueError(f"{src} does not exist!")
 
-    if exists(dest):
-        if overwrite_method == "error":
-            raise ValueError(f"{dest} already exists!")
-        elif overwrite_method == "warn":
-            logger.warning("%s already exists and will be overwritten with %s", dest, src)
-        elif overwrite_method == "skip":
-            logger.warning("%s already exists, no copy from %s will occur", dest, src)
-            return dict()
-        else:
-            return dict()
+    if is_directory(file_properties):
+        raise ValueError(f"{src} is a directory!")
 
     src_blob_client = get_global_blob_container_client(src.sa, src.container).get_blob_client(src.path)
     dest_blob_client = get_global_blob_container_client(dest.sa, dest.container).get_blob_client(
         dest.path
     )
+
+    def md5s_exist_and_are_equal() -> bool:
+        if (
+            src_md5 := src_blob_client.get_blob_properties().content_settings.content_md5
+        ) and src_md5 == dest_blob_client.get_blob_properties().content_settings.content_md5:
+            return True
+        return False
+
+    if dest_blob_client.exists():
+        if md5s_exist_and_are_equal():
+            # no point in copying if the files are the same
+            logger.info(
+                "%s already exists with the same md5 as the file at %s, no copy will occur", dest, src
+            )
+            return CopyInfo(src, dest, dict())
+
+        logger.info(
+            "%s already exists but it and/or the file at %s have unknown md5s, or the md5s differ. "
+            "The overwrite method provided will determine the copy behavior.",
+            dest,
+            src,
+        )
+
+        if overwrite_method == "error":
+            raise ValueError(f"{dest} already exists!")
+        elif overwrite_method == "warn":
+            logger.warning("%s will be overwritten with the file from %s", dest, src)
+        elif overwrite_method == "skip":
+            logger.warning("%s already exists, skipping copy from %s", dest, src)
+            return CopyInfo(src, dest, dict())
 
     sas_token = gen_blob_sas_token(
         src, account_key=account_key, permissions=BlobSasPermissions(read=True)
@@ -56,14 +87,16 @@ def _copy_file(
     # we may not always need a SAS token for the copy, but it would be hard to reason about when they are(n't)
 
     logger.info("Copying %s to %s...", src, dest)
-    return dest_blob_client.start_copy_from_url(f"{src_blob_client.url}?{sas_token}")
+    return CopyInfo(
+        src, dest, dest_blob_client.start_copy_from_url(f"{src_blob_client.url}?{sas_token}")
+    )
 
 
-def wait_for_copy(fqn: AdlsFqn, timeout: int = 300) -> AdlsFqn:
+def wait_for_copy(fqn: AdlsFqn, timeout: int = _WAIT_TIMEOUT) -> AdlsFqn:
     """Set timeout to 0 or a negative int to wait indefinitely."""
-    start_time = time.time()
+    start_time = time.monotonic()
 
-    while (time.time() - start_time < timeout) if timeout > 0 else True:
+    while (time.monotonic() - start_time < timeout) if timeout > 0 else True:
         blob_copy_props = get_blob_properties(fqn).copy
         # blob copy properties actually show copy progress so we could add a progress bar
 
@@ -89,11 +122,11 @@ def copy_file(
     dest: UriIsh,
     overwrite_method: OverwriteMethod = "error",
     wait: bool = True,
-    timeout: int = _COPY_TIMEOUT,
+    timeout: int = _WAIT_TIMEOUT,
     get_account_key: ty.Callable[
         [BlobServiceClient], ty.Union[str, UserDelegationKey]
     ] = get_user_delegation_key,
-) -> CopyRequest:
+) -> CopyInfo:
     src_fqn = parse_any(src)
     dest_fqn = parse_any(dest)
 
@@ -105,28 +138,28 @@ def copy_file(
     )
 
     if wait:
-        copy_props = func()
+        copy_info = func()
         wait_for_copy(dest_fqn, timeout=timeout)
         logger.info("Finished copying %s to %s", src_fqn, dest_fqn)
-        return copy_props
+        return copy_info
 
     return func()
 
 
 def copy_files(
-    srcs_to_dests: ty.Mapping[UriIsh, UriIsh],
+    src_dest_pairs: ty.Collection[ty.Tuple[UriIsh, UriIsh]],
     overwrite_method: OverwriteMethod = "error",
     wait: bool = True,
-    timeout: int = _COPY_TIMEOUT,
+    timeout: int = _WAIT_TIMEOUT,
     get_account_key: ty.Callable[
         [BlobServiceClient], ty.Union[str, UserDelegationKey]
     ] = get_user_delegation_key,
-) -> ty.Dict[AdlsFqn, CopyRequest]:
-    src_fqns_to_dest_fqns = {parse_any(k): parse_any(v) for k, v in srcs_to_dests.items()}
+) -> ty.List[CopyInfo]:
+    src_dest_fqn_pairs = [(parse_any(src), parse_any(dest)) for src, dest in src_dest_pairs]
     get_account_key = cache.locking(get_account_key)
 
-    def copy_wrapper(src: AdlsFqn, dest: AdlsFqn) -> ty.Tuple[AdlsFqn, CopyRequest]:
-        return dest, copy_file(
+    def copy_wrapper(src: AdlsFqn, dest: AdlsFqn) -> CopyInfo:
+        return copy_file(
             src,
             dest,
             overwrite_method=overwrite_method,
@@ -136,8 +169,8 @@ def copy_files(
         )
 
     # would be cool to do this async, but using threads for quicker dev
-    return dict(
+    return sorted(  # just sorting for determinism of output
         parallel.yield_results(
-            [thunks.thunking(copy_wrapper)(src, dest) for src, dest in src_fqns_to_dest_fqns.items()]
+            [thunks.thunking(copy_wrapper)(src, dest) for src, dest in src_dest_fqn_pairs]
         )
     )
--- libs/adls/tests/test_adls/test_copy.py
+++ libs/adls/tests/test_adls/test_copy.py
@@ -44,6 +44,7 @@ def test_unit_wait_for_copy_fails(
 
 
 _FILE_TO_COPY_PATH = "test/read-only/DONT_DELETE_THESE_FILES.txt"
+_FILE_TO_COPY_OVERWRITE_PATH = "test/read-only/integration_nppes.zip"
 
 
 @pytest.fixture
@@ -51,33 +52,56 @@ def copy_file_setup(
     test_remote_root: fqn.AdlsRoot,
     tmp_remote_root: fqn.AdlsRoot,
     random_test_file_path: str,
-) -> ty.Iterator[ty.Tuple[fqn.AdlsFqn, fqn.AdlsFqn, copy.CopyRequest]]:
+) -> ty.Iterator[copy.CopyInfo]:
     tmp_fs = ADLSFileSystem(*tmp_remote_root)
     src = fqn.AdlsFqn(*test_remote_root, path=_FILE_TO_COPY_PATH)
     dest = fqn.AdlsFqn(*tmp_remote_root, path=random_test_file_path)
 
-    yield src, dest, copy.copy_file(src, dest)
+    yield copy.copy_file(src, dest)
 
     tmp_fs.delete_file(dest.path)
 
 
-def test_integration_copy_file(
-    copy_file_setup: ty.Tuple[fqn.AdlsFqn, fqn.AdlsFqn, copy.CopyRequest]
-) -> None:
-    src, dest, copy_request = copy_file_setup  # setup does an initial copy
-    src_blob_props = file_properties.get_blob_properties(src)
-    dest_blob_props = file_properties.get_blob_properties(dest)
+@pytest.fixture
+def file_to_overwrite_with(test_remote_root: fqn.AdlsRoot) -> fqn.AdlsFqn:
+    return test_remote_root / _FILE_TO_COPY_OVERWRITE_PATH
 
-    assert copy_request
+
+def test_integration_copy_file(copy_file_setup: copy.CopyInfo) -> None:
+    copy_info = copy_file_setup  # setup does an initial copy
+    src_blob_props = file_properties.get_blob_properties(copy_info.src)
+    dest_blob_props = file_properties.get_blob_properties(copy_info.dest)
+
+    assert copy_info.copy_occurred
     assert dest_blob_props.copy.status == "success"
     assert dest_blob_props.content_settings.content_md5 == src_blob_props.content_settings.content_md5
 
 
-def test_integration_copy_file_silent_overwrite(
-    copy_file_setup: ty.Tuple[fqn.AdlsFqn, fqn.AdlsFqn, copy.CopyRequest]
+def test_integration_copy_file_same_src_dest(
+    caplog: pytest.LogCaptureFixture,
+    copy_file_setup: copy.CopyInfo,
 ) -> None:
     src, dest, copy_request1 = copy_file_setup  # setup does an initial copy
-    copy_request2 = copy.copy_file(src, dest, overwrite_method="warn")
+
+    with caplog.at_level(logging.INFO):
+        _, _, copy_request2 = copy.copy_file(
+            src, dest
+        )  # dest is already the same as src, no copy will actually happen
+        assert "same md5" in caplog.text
+
+    assert not copy_request2
+
+
+def test_integration_copy_file_silent_overwrite(
+    copy_file_setup: copy.CopyInfo,
+    file_to_overwrite_with: fqn.AdlsFqn,
+) -> None:
+    src, dest, copy_request1 = copy_file_setup  # setup does an initial copy
+    _, _, copy_request2 = copy.copy_file(
+        file_to_overwrite_with,
+        dest,
+        overwrite_method="silent",
+    )
 
     assert copy_request2
     assert copy_request2["last_modified"] >= copy_request1["last_modified"]  # type: ignore[operator]
@@ -86,19 +110,19 @@ def test_integration_copy_file_silent_overwrite(
     dest_blob_props = file_properties.get_blob_properties(dest)
 
     assert dest_blob_props.copy.status == "success"
-    assert dest_blob_props.content_settings.content_md5 == src_blob_props.content_settings.content_md5
+    assert dest_blob_props.content_settings.content_md5 != src_blob_props.content_settings.content_md5
 
 
 def test_integration_copy_file_warn_on_overwrite(
     caplog: pytest.LogCaptureFixture,
     copy_file_setup: ty.Tuple[fqn.AdlsFqn, fqn.AdlsFqn, copy.CopyRequest],
+    file_to_overwrite_with: fqn.AdlsFqn,
 ) -> None:
     src, dest, copy_request1 = copy_file_setup  # setup does an initial copy
-    print(copy_request1)
 
     with caplog.at_level(logging.WARNING):
-        copy_request2 = copy.copy_file(src, dest, overwrite_method="warn")
-        assert "already exists and will be overwritten" in caplog.text
+        _, _, copy_request2 = copy.copy_file(file_to_overwrite_with, dest, overwrite_method="warn")
+        assert "will be overwritten" in caplog.text
 
     assert copy_request2
     assert copy_request2["last_modified"] >= copy_request1["last_modified"]  # type: ignore[operator]
@@ -107,27 +131,35 @@ def test_integration_copy_file_warn_on_overwrite(
     dest_blob_props = file_properties.get_blob_properties(dest)
 
     assert dest_blob_props.copy.status == "success"
-    assert dest_blob_props.content_settings.content_md5 == src_blob_props.content_settings.content_md5
+    assert dest_blob_props.content_settings.content_md5 != src_blob_props.content_settings.content_md5
 
 
 def test_integration_copy_file_skip_overwrite(
     caplog: pytest.LogCaptureFixture,
     copy_file_setup: ty.Tuple[fqn.AdlsFqn, fqn.AdlsFqn, copy.CopyRequest],
+    file_to_overwrite_with: fqn.AdlsFqn,
 ) -> None:
     src, dest, copy_request1 = copy_file_setup  # setup does an initial copy
 
     with caplog.at_level(logging.WARNING):
-        copy_request2 = copy.copy_file(src, dest, overwrite_method="skip")
-        assert "already exists, no copy" in caplog.text
+        _, _, copy_request2 = copy.copy_file(file_to_overwrite_with, dest, overwrite_method="skip")
+        assert "already exists, skipping" in caplog.text
 
     assert copy_request1
     assert not copy_request2
 
+    src_blob_props = file_properties.get_blob_properties(src)
+    dest_blob_props = file_properties.get_blob_properties(dest)
+
+    assert dest_blob_props.copy.status == "success"
+    assert dest_blob_props.content_settings.content_md5 == src_blob_props.content_settings.content_md5
+
 
 def test_integration_copy_file_error_on_overwrite(
-    copy_file_setup: ty.Tuple[fqn.AdlsFqn, fqn.AdlsFqn, copy.CopyRequest]
+    copy_file_setup: ty.Tuple[fqn.AdlsFqn, fqn.AdlsFqn, copy.CopyRequest],
+    file_to_overwrite_with: fqn.AdlsFqn,
 ) -> None:
     src, dest, copy_request = copy_file_setup  # setup does an initial copy
 
     with pytest.raises(ValueError):
-        copy.copy_file(src, dest, overwrite_method="error")
+        copy.copy_file(file_to_overwrite_with, dest, overwrite_method="error")
