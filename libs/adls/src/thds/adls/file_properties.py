a1c0c6d89d069e924d427ca049ad53253b23f842
Tim Dobbins <tmthyjames@gmail.com>
2025-02-04 16:16:40 -0600

    Merge branch 'main' into feature/practicing-specialty/adjust-sampling-policy
    GitOrigin-RevId: 0ba642ac10f6322c99f1c3db7ea73b9acedc038b

--- libs/adls/src/thds/adls/file_properties.py
+++ libs/adls/src/thds/adls/file_properties.py
@@ -1,7 +1,10 @@
+from azure.core.exceptions import AzureError, ResourceNotFoundError
+from azure.storage.blob import BlobProperties
 from azure.storage.filedatalake import FileProperties
 
+from .errors import translate_azure_error
 from .fqn import AdlsFqn
-from .global_client import get_global_fs_client
+from .global_client import get_global_blob_container_client, get_global_fs_client
 
 
 def is_directory(info: FileProperties) -> bool:
@@ -11,3 +14,28 @@ def is_directory(info: FileProperties) -> bool:
 
 def get_file_properties(fqn: AdlsFqn) -> FileProperties:
     return get_global_fs_client(fqn.sa, fqn.container).get_file_client(fqn.path).get_file_properties()
+
+
+def get_blob_properties(fqn: AdlsFqn) -> BlobProperties:
+    return (
+        get_global_blob_container_client(fqn.sa, fqn.container)
+        .get_blob_client(fqn.path)
+        .get_blob_properties()
+    )
+
+
+# At some point it may make sense to separate file and blob property modules,
+# but they also are very closely tied together. AFAIK all files are blobs, and given our usage of ADLS,
+# I don't know if we ever deal with things that are blobs but not files.
+
+
+def exists(fqn: AdlsFqn) -> bool:
+    try:
+        get_blob_properties(fqn)
+        # could generally use `get_file_properties` interchangeably,
+        # but blobs are a lower-level primitive than the ADLS file abstraction
+        return True
+    except ResourceNotFoundError:
+        return False
+    except AzureError as err:
+        translate_azure_error(get_global_fs_client(fqn.sa, fqn.container), fqn.path, err)
