7140552f80cc56225fb782d7a6c9378a806ff5ea
Cailey Kerley <cailey.kerley@trillianthealth.com>
2025-02-06 18:14:35 -0500

    merge main
    GitOrigin-RevId: 77cf4b81a0c904a51153819281f47b08c81997c0

--- libs/adls/tests/test_adls/test_download.py
+++ libs/adls/tests/test_adls/test_download.py
@@ -8,9 +8,9 @@ from uuid import uuid4
 
 import pytest
 from azure.identity.aio import DefaultAzureCredential
-from azure.storage.filedatalake import FileProperties, aio
+from azure.storage.filedatalake import FileProperties, FileSystemClient, aio
 
-from thds.adls import ADLSFileSystem, AdlsFqn
+from thds.adls import ADLSFileSystem, AdlsFqn, AdlsRoot
 from thds.adls.cached_up_down import download_to_cache
 from thds.adls.download import (
     MD5MismatchError,
@@ -27,13 +27,6 @@ from thds.adls.ro_cache import Cache, global_cache
 
 __TMPCACHEDIR = TemporaryDirectory(prefix="cache-for-adls-tests--")
 _TEST_CACHE = Cache(Path(__TMPCACHEDIR.name) / ".adls-md5-ro-cache", True)
-__TMPDIR = TemporaryDirectory(prefix="for-adls-tests--")
-_TEST_DEST = Path(__TMPDIR.name)
-
-_TEST_REMOTE = "thdsdatasets", "prod-datasets"
-_TMP_REMOTE = "thdsscratch", "tmp"
-global_client = get_global_fs_client(*_TEST_REMOTE)
-# this location for test data has been blessed by Matt Eby himself.
 
 
 def test_unit_download_coroutine_does_not_accept_empty_path():
@@ -41,11 +34,11 @@ def test_unit_download_coroutine_does_not_accept_empty_path():
         _download_or_use_verified_cached_coroutine(AdlsFqn("foo", "bar", "baz"), "").send(None)
 
 
-def test_unit_download_coroutine_no_cache_no_remote_md5b64():
+def test_unit_download_coroutine_no_cache_no_remote_md5b64(test_dest: Path):
     """without an md5, file properties will be requested. with no
     remote md5, no md5 checks will be performed and no cache will be used."""
     fake = AdlsFqn.parse("adls://does/not/exist.lol")
-    co = _download_or_use_verified_cached_coroutine(fake, _TEST_DEST / "exist.lol", cache=_TEST_CACHE)
+    co = _download_or_use_verified_cached_coroutine(fake, test_dest / "exist.lol", cache=_TEST_CACHE)
 
     request = co.send(None)
     assert request == _IoRequest.FILE_PROPERTIES
@@ -65,74 +58,80 @@ def test_unit_download_coroutine_no_cache_no_remote_md5b64():
 
     assert not si.value.value.hit  # not a cache hit
     assert _TEST_CACHE.path(fake).exists()  # file _was_ downloaded _into_ cache, however.
-    assert (_TEST_DEST / "exist.lol").exists()  # file also downloaded locally
+    assert (test_dest / "exist.lol").exists()  # file also downloaded locally
 
 
-def test_integration_download_to_local_and_reuse_from_there():
+def test_integration_download_to_local_and_reuse_from_there(
+    global_test_fs_client: FileSystemClient, test_dest: Path
+):
     real = "test/read-only/DONT_DELETE_THESE_FILES.txt"
-    fqn = AdlsFqn(global_client.account_name, global_client.file_system_name, real)
+    fqn = AdlsFqn(global_test_fs_client.account_name, global_test_fs_client.file_system_name, real)
     md5b64 = "U3vtigRGuroWtJFEQ5dKoQ=="
-    lcl = _TEST_DEST / "DONT_DELETE_THESE_FILES.txt"
-    hit = download_or_use_verified(global_client, real, lcl, md5b64)
+    lcl = test_dest / "DONT_DELETE_THESE_FILES.txt"
+    hit = download_or_use_verified(global_test_fs_client, real, lcl, md5b64)
     assert not hit
     assert lcl.exists()
     assert not _TEST_CACHE.path(fqn).exists()
 
-    hit = download_or_use_verified(global_client, real, lcl, md5b64)
+    hit = download_or_use_verified(global_test_fs_client, real, lcl, md5b64)
     assert hit
     assert lcl.exists()
     assert not _TEST_CACHE.path(fqn).exists()
 
 
-def test_integration_download_to_cache_with_no_expected_md5_and_reuse_from_there():
+def test_integration_download_to_cache_with_no_expected_md5_and_reuse_from_there(
+    global_test_fs_client: FileSystemClient, test_dest: Path
+):
     remote = "test/read-only/DONT_DELETE_THESE_FILES.txt"
-    fqn = AdlsFqn(global_client.account_name, global_client.file_system_name, remote)
+    fqn = AdlsFqn(global_test_fs_client.account_name, global_test_fs_client.file_system_name, remote)
     md5b64 = ""
-    lcl = _TEST_DEST / "DONT_DELETE_THESE_FILES----use-cache.txt"
-    hit = download_or_use_verified(global_client, remote, lcl, md5b64, cache=_TEST_CACHE)
+    lcl = test_dest / "DONT_DELETE_THESE_FILES----use-cache.txt"
+    hit = download_or_use_verified(global_test_fs_client, remote, lcl, md5b64, cache=_TEST_CACHE)
     assert not hit
     assert lcl.exists()
     assert _TEST_CACHE.path(fqn).exists()
 
-    hit = download_or_use_verified(global_client, remote, lcl, md5b64, cache=_TEST_CACHE)
+    hit = download_or_use_verified(global_test_fs_client, remote, lcl, md5b64, cache=_TEST_CACHE)
     assert hit
     assert lcl.exists()
     assert _TEST_CACHE.path(fqn).exists()
 
-    newlcl = _TEST_DEST / "DONT_DELETE---put-in-different-place-but-use-cache.txt"
-    hit = download_or_use_verified(global_client, remote, newlcl, md5b64, cache=_TEST_CACHE)
+    newlcl = test_dest / "DONT_DELETE---put-in-different-place-but-use-cache.txt"
+    hit = download_or_use_verified(global_test_fs_client, remote, newlcl, md5b64, cache=_TEST_CACHE)
     assert newlcl.exists()
     assert lcl.exists()  # still...
     assert _TEST_CACHE.path(fqn).exists()
 
 
-def test_integration_handles_emoji_and_long_key():
+def test_integration_handles_emoji_and_long_key(
+    global_test_fs_client: FileSystemClient, test_dest: Path
+):
     # this key is longer than 255 bytes, which is longer than most
     # local filesystems can accept.  therefore, we must truncate it in
     # a reliably-discoverable way.
     remote = "test/read-only/😀aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"  # noqa
-    fqn = AdlsFqn(global_client.account_name, global_client.file_system_name, remote)
+    fqn = AdlsFqn(global_test_fs_client.account_name, global_test_fs_client.file_system_name, remote)
     md5b64 = "gEL83AfKoP2e3O1Y4RsBqQ=="
-    lcl = _TEST_DEST / "benchmark_hashing.py"
-    hit = download_or_use_verified(global_client, remote, lcl, md5b64, cache=_TEST_CACHE)
+    lcl = test_dest / "benchmark_hashing.py"
+    hit = download_or_use_verified(global_test_fs_client, remote, lcl, md5b64, cache=_TEST_CACHE)
     assert not hit
     assert lcl.exists()
     assert _TEST_CACHE.path(fqn).exists()
     assert "md5-ff6343d3a291258f48cbe455145cbbfa-" in str(_TEST_CACHE.path(fqn))
 
 
-def test_integration_md5_verification():
+def test_integration_md5_verification(global_test_fs_client: FileSystemClient, test_dest: Path):
     real = "test/read-only/DONT_DELETE_THESE_FILES.txt"
     md5b64 = "incorrect-MrMjF87w3GvA=="
-    lcl = _TEST_DEST / "DONT_DELETE_THESE_FILES.txt"
+    lcl = test_dest / "DONT_DELETE_THESE_FILES.txt"
     with pytest.raises(MD5MismatchError):
-        download_or_use_verified(global_client, real, lcl, md5b64)
+        download_or_use_verified(global_test_fs_client, real, lcl, md5b64)
 
 
-def test_unit_md5_verification():
+def test_unit_md5_verification(test_dest: Path):
     made_it = False
     with pytest.raises(MD5MismatchError):
-        local_dest = _TEST_DEST / "a-file.txt"
+        local_dest = test_dest / "a-file.txt"
         with open(local_dest, "w") as f:
             f.write("hi")
         with _verify_md5s_before_and_after_download(
@@ -153,11 +152,11 @@ def _async_adls_fs_client(storage_account: str, container: str) -> aio.FileSyste
 
 
 @pytest.mark.asyncio
-async def test_integration_async():
+async def test_integration_async(test_remote_root: AdlsRoot, test_dest: Path):
     remote = "test/read-only/DONT_DELETE_THESE_FILES.txt"
-    lcl = _TEST_DEST / "DONT_DELETE_THESE_FILES----use-async.txt"
+    lcl = test_dest / "DONT_DELETE_THESE_FILES----use-async.txt"
 
-    async_client = _async_adls_fs_client(*_TEST_REMOTE)
+    async_client = _async_adls_fs_client(*test_remote_root)
     hit = await async_download_or_use_verified(async_client, remote, lcl, "U3vtigRGuroWtJFEQ5dKoQ==")
     assert not hit
     assert lcl.exists()
@@ -168,27 +167,29 @@ async def test_integration_async():
 
 
 @pytest.mark.asyncio
-async def test_file_missing_md5_gets_one_assigned_after_download():
-    fs_client = _async_adls_fs_client(*_TMP_REMOTE)
+async def test_file_missing_md5_gets_one_assigned_after_download(
+    tmp_remote_root: AdlsRoot, test_dest: Path
+):
+    fs_client = _async_adls_fs_client(*tmp_remote_root)
     key = "test/writable/missing-md5.txt"
     file_client = fs_client.get_file_client(key)
     await file_client.upload_data(b"hi-i-have-no-md5", overwrite=True)
     fp = await file_client.get_file_properties()
     assert not fp.content_settings.content_md5
 
-    cache_hit = await async_download_or_use_verified(fs_client, key, _TEST_DEST / "missing-md5.txt")
+    cache_hit = await async_download_or_use_verified(fs_client, key, test_dest / "missing-md5.txt")
     assert not cache_hit
 
     fp = await file_client.get_file_properties()
     assert b64(fp.content_settings.content_md5) == "8Wz15VCq6d73Z0+KUDNqVg=="
 
     # should not error since the md5 should be correct
-    cache_hit = await async_download_or_use_verified(fs_client, key, _TEST_DEST / "missing-md5.txt")
+    cache_hit = await async_download_or_use_verified(fs_client, key, test_dest / "missing-md5.txt")
     assert cache_hit
 
 
-def test_file_with_md5_doesnt_try_to_set_it(caplog):
-    fs = ADLSFileSystem("thdsscratch", "tmp")
+def test_file_with_md5_doesnt_try_to_set_it(caplog: pytest.LogCaptureFixture, tmp_remote_root: AdlsRoot):
+    fs = ADLSFileSystem(*tmp_remote_root)
     key = f"test/thds.adls/file_with_md5_doesnt_try_to_set_it/{uuid4().hex}"
     fs.put_file(Path(__file__).parent.parent / "data" / "hello_world.txt", key)
 
@@ -203,24 +204,26 @@ def test_file_with_md5_doesnt_try_to_set_it(caplog):
 
 
 @pytest.fixture
-def random_test_file_fqn() -> ty.Iterator[AdlsFqn]:
-    fs = ADLSFileSystem("thdsscratch", "tmp")
-    random_part = "random-test-file-fqn-" + uuid4().hex
-    key = f"test/thds.adls/{random_part}"
-    fqn = AdlsFqn(fs.account_name, fs.file_system, key)
+def random_test_file_fqn(
+    test_dest: Path, tmp_remote_root: AdlsRoot, random_test_file_path: str
+) -> ty.Iterator[AdlsFqn]:
+    fs = ADLSFileSystem(*tmp_remote_root)
+    fqn = AdlsFqn(fs.account_name, fs.file_system, random_test_file_path)
 
-    random_file = __TMPDIR.name + "/random.txt"
+    random_file = test_dest / "random.txt"
     with open(random_file, "w") as f:
         f.write(uuid4().hex)
 
-    fs.put_file(random_file, key)  # non-cached upload
+    fs.put_file(random_file, random_test_file_path)  # non-cached upload
 
     yield fqn
 
-    fs.delete_file(key)  # clean up remote
+    fs.delete_file(fqn.path)  # clean up remote
 
 
-def test_parallel_downloads_only_perform_a_single_download(caplog, random_test_file_fqn: AdlsFqn):
+def test_parallel_downloads_only_perform_a_single_download(
+    caplog: pytest.LogCaptureFixture, random_test_file_fqn: AdlsFqn
+):
     with caplog.at_level(logging.DEBUG):
         # we're not actually coordinating via shared memory,
         # but the easiest way to be able to configure the logs
@@ -244,7 +247,7 @@ def test_parallel_downloads_only_perform_a_single_download(caplog, random_test_f
     assert reuse_count == 9
 
 
-def test_clean_download_locks(caplog):
+def test_clean_download_locks(caplog: pytest.LogCaptureFixture):
     num_deleted = _clean_download_locks()
     print("deleted num lockfiles: ", num_deleted)
 
