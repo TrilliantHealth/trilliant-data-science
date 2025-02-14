9ce16af6858f8c8af0393d362902ecdf7e97ac79
Peter Gaultney <petergaultney@gmail.com>
2025-02-13 21:52:26 -0500

    split source.py into several submodules of core.source
    GitOrigin-RevId: e1edfcc53746d2675648431b21681982ae77b2a7

--- libs/core/src/thds/core/source/_construct.py
+++ libs/core/src/thds/core/source/_construct.py
@@ -1,171 +1,12 @@
-"""Wrap openable, read-only data that is either locally-present or downloadable,
-
-yet will not be downloaded (if non-local) until it is actually opened or unwrapped.
-"""
-
-import os
 import typing as ty
-from dataclasses import dataclass
 from functools import partial
 from pathlib import Path
 
-from . import log
-from .files import is_file_uri, path_from_uri, to_uri
-from .hash_cache import filehash
-from .hashing import Hash
-from .types import StrOrPath
-
-
-class Downloader(ty.Protocol):
-    def __call__(self, hash: ty.Optional[Hash]) -> Path:
-        """Closure over a URI that downloads a file to a local path and returns the path.
-        The file may be placed anywhere as long as the file will be readable until the
-        program exits.
-
-        If the URI points to a missing file, this MUST raise any Exception that the
-        underlying implementation desires. It MUST NOT return a Path pointing to a
-        non-existent file.
-
-        The Hash may be used to short-circuit a download that would result in downloading
-        a file that does not match the expected hash, but the Downloader need not verify
-        the Hash of the file downloaded after the fact, as that will be performed by
-        default by the Source object.
-        """
-
-
-class DownloadHandler(ty.Protocol):
-    def __call__(self, uri: str) -> ty.Optional[Downloader]:
-        """Returns a Downloader containing the URI if this URI can be handled.  Returns
-        None if this URI cannot be handled.
-        """
-
-
-def _LocalFileHandler(uri: str) -> ty.Optional[Downloader]:
-    if not is_file_uri(uri):
-        return None
-
-    def download_file(hash: ty.Optional[Hash]) -> Path:
-        lpath = path_from_uri(uri)
-        if not lpath.exists():
-            raise FileNotFoundError(lpath)
-        if hash:
-            _check_hash(hash, lpath)
-        return lpath
-
-    return download_file
-
-
-def register_download_handler(key: str, handler: DownloadHandler):
-    # key is not currently used for anything other than avoiding
-    # having duplicates registered for whatever reason.
-    _DOWNLOAD_HANDLERS[key] = handler
-
-
-_DOWNLOAD_HANDLERS: ty.Dict[str, DownloadHandler] = dict()
-register_download_handler("local_file", _LocalFileHandler)
-
-
-def _get_download_handler(uri: str) -> Downloader:
-    for handler in _DOWNLOAD_HANDLERS.values():
-        if downloader := handler(uri):
-            return downloader
-    raise ValueError(f"No SourcePath download handler for uri: {uri}")
-
-
-class SourceHashMismatchError(ValueError):
-    pass
-
-
-def _check_hash(expected_hash: ty.Optional[Hash], path: Path) -> Hash:
-    hash_algo = expected_hash.algo if expected_hash else "sha256"
-    with log.logger_context(hash_for=f"source-{hash_algo}"):
-        computed_hash = filehash(hash_algo, path)
-    if expected_hash and expected_hash != computed_hash:
-        raise SourceHashMismatchError(
-            f"{expected_hash.algo} mismatch for {path};"
-            f" got {computed_hash.bytes!r}, expected {expected_hash.bytes!r}"
-        )
-    return computed_hash
-
-
-@dataclass(frozen=True)
-class Source(os.PathLike):
-    """Source is meant to be a consistent in-memory representation for an abstract,
-    **read-only** source of data that may not be present locally when an application
-    starts.
-
-    A Source uses `os.PathLike` (`__fspath__`) to support transparent `open(src)` calls,
-    so in many cases it will be a drop-in replacement for Path or str filenames. If you
-    need an actual Path object, you can call `path()` to get one, but you should prefer to
-    defer this until the actual location of use.
-
-    By 'wrapping' read-only data in these objects, we can unify the code around how we
-    unwrap and use them, which should allow us to more easily support different execution
-    environments and sources of data.
-
-    For instance, a Source could be a file on disk, but it could also be a file in
-    ADLS.
-
-    Furthermore, libraries which build on top of this one may use this representation to
-    identify opportunities for optimization, by representing the Source in a stable
-    and consistent format that allows different underlying data sources to fulfill the
-    request for the data based on environmental context. A library could choose to
-    transparently transform a local-path-based Source into a Source representing a
-    remote file, without changing the semantics of the object as observed by the code.
-
-    One reason a Hash is part of the interface is so that libraries interacting with the
-    object can use the hash as a canonical 'name' for the data, if one is available.
-
-    Another reason is that we can add a layer of consistency checking to data we're
-    working with, at the cost of a few compute cycles. Since Sources are meant to represent
-    read-only data, the Hash is a meaningful and persistent marker of data identity.
-
-    Do not call its constructor in application code. Use `from_file` or `from_uri` instead.
-    """
-
-    uri: str
-    hash: ty.Optional[Hash] = None
-    # hash and equality are based only on the _identity_ of the object,
-    # not on the other properties that provide some caching functionality.
-
-    @property
-    def cached_path(self) -> ty.Optional[Path]:
-        """This is part of the public interface as far as checking to see whether a file
-        is already present locally, but its existence and value is not part of equality or
-        the hash for this class - it exists purely as an optimization.
-        """
-        return getattr(self, "__cached_path", None)
-
-    def _set_cached_path(self, lpath: ty.Optional[Path]):
-        """protected interface for setting a cached Path since the attribute is not
-        available via the constructor.
-        """
-        super().__setattr__("__cached_path", lpath)  # this works around dataclass.frozen.
-        # https://noklam.github.io/blog/posts/2022-04-22-python-dataclass-partiala-immutable.html
-
-    def path(self) -> Path:
-        """Any Source can be turned into a local file path.
-
-        Remember that the resulting data is meant to be read-only. If you want to mutate
-        the data, you should first make a copy.
-
-        If not already present locally, this will incur a one-time download. Then, if the
-        Source has a Hash, the Hash will be validated against the downloaded file, and a
-        failure will raise SourceHashMismatchError.
-        """
-        if self.cached_path is None or not self.cached_path.exists():
-            lpath = _get_download_handler(self.uri)(self.hash)
-            # path() used to be responsible for checking the hash, but since we pass it to the downloader,
-            # it really makes more sense to allow the downloader to decide how to verify its own download,
-            # and we don't want to duplicate any effort that it may have already put in.
-            self._set_cached_path(lpath)
-
-        assert self.cached_path and self.cached_path.exists()
-        return self.cached_path
-
-    def __fspath__(self) -> str:
-        return os.fspath(self.path())
-
+from ..files import is_file_uri, path_from_uri, to_uri
+from ..hashing import Hash
+from ..types import StrOrPath
+from . import _download
+from .src import Source
 
 # Creation from local Files or from remote URIs
 
@@ -183,9 +24,9 @@ def from_file(filename: StrOrPath, hash: ty.Optional[Hash] = None, uri: str = ""
         raise FileNotFoundError(path)
 
     if uri:
-        src = from_uri(uri, _check_hash(hash, path))
+        src = from_uri(uri, _download._check_hash(hash, path))
     else:
-        src = Source(to_uri(path), _check_hash(hash, path))
+        src = Source(to_uri(path), _download._check_hash(hash, path))
     src._set_cached_path(path)  # internally, it's okay to hack around immutability.
     return src
 
---
+++ libs/core/src/thds/core/source/serde.py
@@ -0,0 +1,105 @@
+# this should later get promoted somewhere, probably
+import json
+import typing as ty
+from functools import partial
+from pathlib import Path
+
+from thds.core import files, hashing, log, types
+
+from . import _construct
+from .src import Source
+
+_SHA256_B64 = "sha256b64"
+_MD5_B64 = "md5b64"
+
+logger = log.getLogger(__name__)
+
+
+def _from_sha256b64(d: dict) -> ty.Optional[hashing.Hash]:
+    if "sha256b64" in d:
+        return hashing.Hash(algo="sha256", bytes=hashing.db64(d[_SHA256_B64]))
+    return None
+
+
+def _from_md5b64(d: dict) -> ty.Optional[hashing.Hash]:
+    if "md5b64" in d:
+        return hashing.Hash(algo="md5", bytes=hashing.db64(d[_MD5_B64]))
+    return None
+
+
+HashParser = ty.Callable[[dict], ty.Optional[hashing.Hash]]
+_BASE_PARSERS = (_from_sha256b64, _from_md5b64)
+
+
+def base_parsers() -> ty.Tuple[HashParser, ...]:
+    return _BASE_PARSERS
+
+
+def from_json(json_source: str, hash_parsers: ty.Collection[HashParser] = base_parsers()) -> Source:
+    d = json.loads(json_source)
+    return _construct.from_uri(
+        uri=d["uri"],
+        hash=next(filter(None, (p(d) for p in hash_parsers)), None),
+    )
+
+
+def _generic_hash_serializer(
+    algo: str, stringify_hash: ty.Callable[[bytes], str], keyname: str, hash: hashing.Hash
+) -> ty.Optional[dict]:
+    if hash.algo == algo:
+        return {keyname: stringify_hash(hash.bytes)}
+    return None
+
+
+_to_sha256b64 = partial(_generic_hash_serializer, "sha256", hashing.b64, _SHA256_B64)
+_to_md5b64 = partial(_generic_hash_serializer, "md5", hashing.b64, _MD5_B64)
+
+HashSerializer = ty.Callable[[hashing.Hash], ty.Optional[dict]]
+_BASE_HASH_SERIALIZERS: ty.Tuple[HashSerializer, ...] = (_to_md5b64, _to_sha256b64)  # type: ignore
+
+
+def base_hash_serializers() -> ty.Tuple[HashSerializer, ...]:
+    return _BASE_HASH_SERIALIZERS
+
+
+def to_json(
+    source: Source, hash_serializers: ty.Collection[HashSerializer] = base_hash_serializers()
+) -> str:
+    hash_dict = (
+        next(filter(None, (ser(source.hash) for ser in hash_serializers if source.hash)), None)
+    ) or dict()
+    return json.dumps(dict(uri=source.uri, **hash_dict))
+
+
+def from_unknown_user_path(path: types.StrOrPath, desired_uri: str) -> Source:
+    """Sometimes you may want to load a Source directly from a Path provided by a user.
+
+    It _might_ represent something loadable as a from_json Source, but it might just be a
+    raw file that needs to be loaded with from_file!
+
+    This is a _reasonable_ (but not guaranteed!) way of trying to ascertain which one it
+    is, and specifying where it should live 'remotely' if such a thing becomes
+    necessary.
+
+    Your application might need to implement something more robust if the
+    actual underlying data is likely to be a JSON blob containing the key `uri`, for
+    instance.
+    """
+    with open(path) as readable:
+        try:
+            return from_json(readable.read(4096))
+        except (json.JSONDecodeError, UnicodeDecodeError):
+            return _construct.from_file(path, uri=desired_uri)
+
+
+def write_to_json_file(source: Source, local_file: Path) -> bool:
+    """Write the canonical JSON serialization of the Source to a file."""
+    local_file.parent.mkdir(parents=True, exist_ok=True)
+    previous_source = local_file.read_text() if local_file.exists() else None
+    new_source = to_json(source) + "\n"
+    if new_source != previous_source:
+        with files.atomic_text_writer(local_file) as f:
+            logger.info(f"Writing {source} to {local_file}")
+            f.write(new_source)
+            return True
+    return False
