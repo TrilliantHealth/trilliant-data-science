710b8afb0f9e900efc68d793b557265756d18143
Peter Gaultney <petergaultney@gmail.com>
2025-02-18 13:22:52 -0500

    Merge pull request #2348 from TrilliantHealth/feature/core/source-tree
    add SourceTree abstraction and logical_root utility
    
    GitOrigin-RevId: 6aa6700b782d76b65b65256eaa49bd70e8217015

---
+++ libs/adls/src/thds/adls/source_tree.py
@@ -0,0 +1,57 @@
+import typing as ty
+from dataclasses import dataclass
+
+from azure.storage.blob import ContainerClient
+
+from thds.core import hashing
+from thds.core.source.tree import SourceTree
+
+from . import fqn, global_client, source, uri
+
+
+@dataclass
+class BlobMeta:
+    path: str
+    size: int
+    md5: ty.Optional[hashing.Hash]
+
+
+# https://learn.microsoft.com/en-us/python/api/azure-storage-blob/azure.storage.blob.containerclient?view=azure-python#azure-storage-blob-containerclient-list-blobs
+# https://learn.microsoft.com/en-us/python/api/azure-storage-blob/azure.storage.blob.blobproperties?view=azure-python
+# https://learn.microsoft.com/en-us/python/api/azure-storage-blob/azure.storage.blob.contentsettings?view=azure-python
+def list_blob_meta(
+    container_client: ContainerClient, root_dir: str, match_suffix: str = ""
+) -> ty.List[BlobMeta]:
+    """Gets the path (relative to the SA/container root), size, and MD5 hash of all blobs in a directory."""
+    return [
+        BlobMeta(
+            blob_props.name,  # type: ignore
+            blob_props.size,  # type: ignore
+            (
+                hashing.Hash("md5", bytes(blob_props.content_settings.content_md5))
+                if blob_props.content_settings.content_md5
+                else None
+            ),
+        )
+        for blob_props in container_client.list_blobs(name_starts_with=root_dir)
+        if blob_props.size > 0  # type: ignore
+        # container client lists directories as blobs with size 0
+        and blob_props.name.endswith(match_suffix)  # type: ignore
+    ]
+
+
+def from_path(adls_path: uri.UriIsh, match_suffix: str = "") -> SourceTree:
+    """Creates a SourceTree object where the logical root is the final piece of the
+    provided adls path.
+    """
+    root_fqn = uri.parse_any(adls_path)
+
+    container_client = global_client.get_global_blob_container_client(root_fqn.sa, root_fqn.container)
+    container_root = root_fqn.root()
+    return SourceTree(
+        sources=[
+            source.from_adls(container_root / blob_meta.path, hash=blob_meta.md5)
+            for blob_meta in list_blob_meta(container_client, root_fqn.path, match_suffix=match_suffix)
+        ],
+        higher_logical_root=fqn.split(root_fqn)[-1],
+    )
---
+++ libs/core/src/thds/core/source/_download.py
@@ -0,0 +1,84 @@
+"""Wrap openable, read-only data that is either locally-present or downloadable,
+
+yet will not be downloaded (if non-local) until it is actually opened or unwrapped.
+"""
+
+import typing as ty
+from pathlib import Path
+
+from .. import log
+from ..files import is_file_uri, path_from_uri
+from ..hash_cache import filehash
+from ..hashing import Hash
+
+
+class Downloader(ty.Protocol):
+    def __call__(self, hash: ty.Optional[Hash]) -> Path:
+        """Closure over a URI that downloads a file to a local path and returns the path.
+        The file may be placed anywhere as long as the file will be readable until the
+        program exits.
+
+        If the URI points to a missing file, this MUST raise any Exception that the
+        underlying implementation desires. It MUST NOT return a Path pointing to a
+        non-existent file.
+
+        The Hash may be used to short-circuit a download that would result in downloading
+        a file that does not match the expected hash, but the Downloader need not verify
+        the Hash of the file downloaded after the fact, as that will be performed by
+        default by the Source object.
+        """
+
+
+class DownloadHandler(ty.Protocol):
+    def __call__(self, uri: str) -> ty.Optional[Downloader]:
+        """Returns a Downloader containing the URI if this URI can be handled.  Returns
+        None if this URI cannot be handled.
+        """
+
+
+def _LocalFileHandler(uri: str) -> ty.Optional[Downloader]:
+    if not is_file_uri(uri):
+        return None
+
+    def download_file(hash: ty.Optional[Hash]) -> Path:
+        lpath = path_from_uri(uri)
+        if not lpath.exists():
+            raise FileNotFoundError(lpath)
+        if hash:
+            _check_hash(hash, lpath)
+        return lpath
+
+    return download_file
+
+
+def register_download_handler(key: str, handler: DownloadHandler):
+    # key is not currently used for anything other than avoiding
+    # having duplicates registered for whatever reason.
+    _DOWNLOAD_HANDLERS[key] = handler
+
+
+_DOWNLOAD_HANDLERS: ty.Dict[str, DownloadHandler] = dict()
+register_download_handler("local_file", _LocalFileHandler)
+
+
+def _get_download_handler(uri: str) -> Downloader:
+    for handler in _DOWNLOAD_HANDLERS.values():
+        if downloader := handler(uri):
+            return downloader
+    raise ValueError(f"No SourcePath download handler for uri: {uri}")
+
+
+class SourceHashMismatchError(ValueError):
+    pass
+
+
+def _check_hash(expected_hash: ty.Optional[Hash], path: Path) -> Hash:
+    hash_algo = expected_hash.algo if expected_hash else "sha256"
+    with log.logger_context(hash_for=f"source-{hash_algo}"):
+        computed_hash = filehash(hash_algo, path)
+    if expected_hash and expected_hash != computed_hash:
+        raise SourceHashMismatchError(
+            f"{expected_hash.algo} mismatch for {path};"
+            f" got {computed_hash.bytes!r}, expected {expected_hash.bytes!r}"
+        )
+    return computed_hash
