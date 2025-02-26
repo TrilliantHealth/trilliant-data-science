45e8f2d1dafa93df596425a7b594bb307f851922
Caleb Gregory <caleb.gregory@trillianthealth.com>
2025-02-26 16:37:32 -0500

    refactors tests to no longer use a sample for df_from_uri. also adds a test to test reading a delta table from ADLS in a DBX workflow. adds delta-rs as a dev-dependency for delta-table dumping
    GitOrigin-RevId: 03fb5ca5495532e52e2fa33baed9ae24ca14985d

--- libs/adls/src/thds/adls/cached_up_down.py
+++ libs/adls/src/thds/adls/cached_up_down.py
@@ -54,7 +54,7 @@ def download_directory(fqn: AdlsFqn) -> Path:
 
 
 def upload_directory_through_cache(dest: UriIsh, src_path: Path) -> SourceTree:
-    if src_path.is_dir():
+    if not src_path.is_dir():
         raise ValueError(f"If you want to upload a file, use {upload_through_cache.__name__} instead")
 
     dest = parse_any(dest)
