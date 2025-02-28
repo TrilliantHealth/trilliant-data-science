6055cac8c58ce1e75e54aa736334550d2f6de821
yzcommitting <yu.zhang@trillianthealth.com>
2025-02-28 10:42:57 -0600

    merge conflicts
    GitOrigin-RevId: 1b23a86d456d3150820783d9d37916967e07d9d1

--- libs/adls/src/thds/adls/download.py
+++ libs/adls/src/thds/adls/download.py
@@ -17,8 +17,8 @@ from thds.core import log, scope, tmp
 from thds.core.hashing import b64
 from thds.core.types import StrOrPath
 
+from . import azcopy
 from ._progress import report_download_progress
-from .conf import CONNECTION_TIMEOUT, DOWNLOAD_FILE_MAX_CONCURRENCY
 from .download_lock import download_lock
 from .errors import MD5MismatchError, translate_azure_error
 from .etag import match_etag
@@ -34,12 +34,14 @@ def _atomic_download_and_move(
     fqn: AdlsFqn,
     dest: StrOrPath,
     properties: ty.Optional[FileProperties] = None,
-) -> ty.Iterator[ty.IO[bytes]]:
+) -> ty.Iterator[azcopy.download.DownloadRequest]:
     with tmp.temppath_same_fs(dest) as dpath:
         with open(dpath, "wb") as f:
             known_size = (properties.size or 0) if properties else 0
             logger.debug("Downloading %s", fqn)
-            yield report_download_progress(f, str(dest), known_size)
+            yield azcopy.download.DownloadRequest(
+                report_download_progress(f, str(fqn), known_size), dpath
+            )
         try:
             os.rename(dpath, dest)  # will succeed even if dest is read-only
         except OSError as oserr:
@@ -139,7 +141,7 @@ class _IoRequest(enum.Enum):
     FILE_PROPERTIES = "file_properties"
 
 
-IoRequest = ty.Union[_IoRequest, ty.IO[bytes]]
+IoRequest = ty.Union[_IoRequest, azcopy.download.DownloadRequest]
 IoResponse = ty.Union[FileProperties, None]
 
 
@@ -206,7 +208,8 @@ def _download_or_use_verified_cached_coroutine(  # noqa: C901
         # we don't know what we expect, so attempt to retrieve an
         # expectation from ADLS itself.
         file_properties = yield _IoRequest.FILE_PROPERTIES
-        md5b64 = _remote_md5b64(file_properties)
+        md5b64 = _remote_md5b64(file_properties)  # type: ignore[arg-type]
+        # TODO - check above type ignore
 
     def attempt_cache_hit() -> ty.Optional[_FileResult]:
         if not md5b64:
@@ -257,7 +260,8 @@ def _download_or_use_verified_cached_coroutine(  # noqa: C901
     file_properties = yield _IoRequest.FILE_PROPERTIES
     # no point in downloading if we've asked for hash X but ADLS only has hash Y.
     with _verify_md5s_before_and_after_download(
-        _remote_md5b64(file_properties),
+        _remote_md5b64(file_properties),  # type: ignore[arg-type]
+        # TODO - check above type ignore
         md5b64,
         fqn,
         local_path,
@@ -291,7 +295,7 @@ def _prep_download_coroutine(
     DataLakeFileClient,
 ]:
     co = _download_or_use_verified_cached_coroutine(
-        AdlsFqn(fs_client.account_name, fs_client.file_system_name, remote_key),
+        AdlsFqn(ty.cast(str, fs_client.account_name), fs_client.file_system_name, remote_key),
         local_path,
         md5b64=md5b64,
         cache=cache,
@@ -304,7 +308,8 @@ def _set_md5_if_missing(
 ) -> ty.Optional[ContentSettings]:
     if not file_properties or file_properties.content_settings.content_md5:
         return None
-    file_properties.content_settings.content_md5 = b64decode(md5b64)
+    file_properties.content_settings.content_md5 = b64decode(md5b64)  # type: ignore[assignment]
+    # TODO - check above type ignore
     return file_properties.content_settings
 
 
@@ -332,12 +337,12 @@ def download_or_use_verified(
                     # only fetch these if they haven't already been requested
                     file_properties = dl_file_client.get_file_properties()
                 co_request = co.send(file_properties)
-            else:  # needs file object
-                dl_file_client.download_file(
-                    max_concurrency=DOWNLOAD_FILE_MAX_CONCURRENCY(),
-                    connection_timeout=CONNECTION_TIMEOUT(),
-                ).readinto(co_request)
+            elif isinstance(co_request, azcopy.download.DownloadRequest):
+                # coroutine is requesting download
+                azcopy.download.sync_fastpath(dl_file_client, co_request)
                 co_request = co.send(None)
+            else:
+                raise ValueError(f"Unexpected coroutine request: {co_request}")
     except StopIteration as si:
         if cs := _set_md5_if_missing(file_properties, si.value.md5b64):
             try:
@@ -368,21 +373,25 @@ async def async_download_or_use_verified(
             if co_request == _IoRequest.FILE_PROPERTIES:
                 if not file_properties:
                     # only fetch these if they haven't already been requested
-                    file_properties = await dl_file_client.get_file_properties()
+                    file_properties = await dl_file_client.get_file_properties()  # type: ignore[misc]
+                    # TODO - check above type ignore
                 co_request = co.send(file_properties)
-            else:  # needs file object
-                reader = await dl_file_client.download_file(
-                    max_concurrency=DOWNLOAD_FILE_MAX_CONCURRENCY(),
-                    connection_timeout=CONNECTION_TIMEOUT(),
-                )
-                await reader.readinto(co_request)
+            elif isinstance(co_request, azcopy.download.DownloadRequest):
+                # coroutine is requesting download
+                await azcopy.download.async_fastpath(dl_file_client, co_request)
                 co_request = co.send(None)
+            else:
+                raise ValueError(f"Unexpected coroutine request: {co_request}")
+
     except StopIteration as si:
         if cs := _set_md5_if_missing(file_properties, si.value.md5b64):
             try:
                 logger.info(f"Setting missing MD5 for {remote_key}")
                 assert file_properties
-                await dl_file_client.set_http_headers(cs, **match_etag(file_properties))
+                await dl_file_client.set_http_headers(  # type: ignore[misc]
+                    cs, **match_etag(file_properties)
+                )
+                # TODO - check above type ignore
             except HttpResponseError as hre:
                 logger.info(f"Unable to set MD5 for {remote_key}: {hre}")
         return si.value.hit
