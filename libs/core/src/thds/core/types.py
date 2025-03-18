d321ff9b66f8fce0ecfbaeb88a1b8ea43b3ab399
Cailey Kerley <cailey.kerley@trillianthealth.com>
2025-03-18 14:35:11 -0400

    Merge branch 'main' into feature/reference-data/caps-whitelisting
    GitOrigin-RevId: aaed4c6c0f2175f066b65f6db6855231bcdc9d4d

--- libs/core/src/thds/core/types.py
+++ libs/core/src/thds/core/types.py
@@ -1,4 +1,6 @@
 import os
 import typing as ty
 
-StrOrPath = ty.Union[str, os.PathLike]
+StrOrPath = ty.Union[
+    str, os.PathLike
+]  # DEPRECATED - please be explicit about this bc it isn't much extra typing
