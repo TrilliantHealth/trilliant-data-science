de4355b8be036a15f0b83c78ee1a521eaec01cd3
Peter Gaultney <petergaultney@gmail.com>
2025-04-09 15:28:47 +0000

    Merge pull request #2582 from TrilliantHealth/bugfix/mops/k8s-job-name-no-prefix
    fix k8s job name when no user prefix
    
    GitOrigin-RevId: d8451a1d16d2b990dfdd7327c1b7e531eade8427

--- libs/mops/tests/test_mops/unit/k8s/test_launch.py
+++ libs/mops/tests/test_mops/unit/k8s/test_launch.py
@@ -8,3 +8,22 @@ def test_job_name_len():
     assert len(name) == 63
     assert name.startswith("a-very-long-prefix-that-")
     assert "-34234234234324-" in name
+
+
+def test_job_name_without_prefix():
+    job_num = (
+        "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+    )
+
+    assert len(job_num) == 90
+    name = construct_job_name("", job_num)
+    assert len(name) == 63
+    assert not name.startswith("-")
+
+
+def test_job_name_prefix_with_invalid_chars_doesnt_leave_leading_or_trailing_dashes():
+    prefix = "^(*&@#$--P*e@T#e$R--"
+
+    name = construct_job_name(prefix, "234234")
+    assert name.startswith("p-e-t-e-r-")
+    assert name[len("p-e-t-e-r-")] != "-"
