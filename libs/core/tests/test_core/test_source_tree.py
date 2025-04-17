33d6aa0ef825a8d190cdbc3bfc2b35fa2a03b5f3
Carlos Arias <carias8@users.noreply.github.com>
2025-04-17 16:43:56 +0000

    Merge pull request #2623 from TrilliantHealth/bugfix/databricks/aiohttp-no-socket-timeout-error
    Bugfix: databricks/adls - aiohttp no socket timeout error
    GitOrigin-RevId: 28861773bfb22e0fb024feae7756821d559d68e5

--- libs/core/tests/test_core/test_source_tree.py
+++ libs/core/tests/test_core/test_source_tree.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
-from thds.core.source import tree
+import pytest
+
+from thds.core import scope
+from thds.core.source import tree, tree_from_directory
 
 
 def test_logical_tree_replication_operations():
@@ -42,3 +46,43 @@ def test_logical_tree_replication_operations():
     # Assertions
     assert logical_dest == expected_logical_dest
     assert operations == expected_operations
+
+
+class Test_tree_from_directory:
+    @scope.bound
+    def test_it_raises_when_given_a_file(self):
+        tmp = Path(scope.enter(TemporaryDirectory()))
+
+        a_file = tmp / "a_file.txt"
+        a_file.touch()
+
+        with pytest.raises(NotADirectoryError):
+            tree_from_directory(a_file)
+
+    @scope.bound
+    def test_it_raises_when_given_a_non_existent_dir(self):
+        tmp = Path(scope.enter(TemporaryDirectory()))
+
+        a_dir = tmp / "a_dir"  # doesn't actually exist
+
+        with pytest.raises(FileNotFoundError):
+            tree_from_directory(a_dir)
+
+    @scope.bound
+    def test_it_returns_a_source_tree_when_given_a_dir(self):
+        tmp = Path(scope.enter(TemporaryDirectory()))
+        file_1 = tmp / "1.txt"
+        file_2 = tmp / "2.txt"
+
+        contents = "SourceTrees are sew kewllll"
+        # the function that takes a hash of the file contents is tested elsewhere.
+        # here, we're just going to assert that the hashes of these two files are equivalent.
+
+        for file in (file_1, file_2):
+            with open(file, "w", encoding="utf-8") as f:
+                f.write(contents)
+
+        source_tree = tree_from_directory(tmp)
+
+        assert len(source_tree.sources) == 2
+        assert source_tree.sources[0].hash == source_tree.sources[1].hash
