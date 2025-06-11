e89f806919a7ac939bba58cc2d3ccf6a0d0ed4a9
Justin Fincher <justin.fincher@trillianthealth.com>
2025-06-10 22:39:06 -0400

    Merge branch 'main' of https://github.com/TrilliantHealth/ds-monorepo into feature/incorporate_qe_into_ua_merge_main
    GitOrigin-RevId: 8a7e9d60340cfb63a247d6a85de262cb74bb2d11

--- libs/mops/scripts/render_echart.py
+++ libs/mops/scripts/render_echart.py
@@ -10,12 +10,21 @@ import hjson  # type: ignore
 from nicegui import ui  # type: ignore
 
 
-@ui.page("/")
-def index():
-    echart_option = hjson.loads(open("echart_graph.json").read())
-
-    with ui.element("div").style("width: fit-content; height: 100vh; margin: auto;"):
+def render(echart_file: str) -> None:
+    @ui.refreshable
+    def render_echart(echart_file: str) -> None:
+        echart_option = hjson.loads(open(echart_file).read())
         ui.echart(echart_option).style("width: 100vw; height: 100vh")
+
+    render_echart(echart_file)
+
+    ui.button("Refresh", on_click=render_echart.refresh).style(
+        "position: absolute; top: 10px; right: 10px"
+    )
+
+
+with ui.element("div").style("width: fit-content; height: 100vh; margin: auto;"):
+    render("echart_graph.json")
 
 
 if __name__ in {"__main__", "__mp_main__"}:
