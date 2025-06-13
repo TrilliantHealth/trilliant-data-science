5f52efa9d9affff36ab936e3cdfb3ab64378da1c
Peter Gaultney <petergaultney@gmail.com>
2025-06-13 00:33:23 -0400

    Merge pull request #2822 from TrilliantHealth/task/mops/improve-echart-graphing
    make a few tweaks to how we graph the mops dags
    
    GitOrigin-RevId: c5f22b7e273164061472ac11563020d804e36714

--- libs/mops/scripts/render_echart.py
+++ libs/mops/scripts/render_echart.py
@@ -10,21 +10,12 @@ import hjson  # type: ignore
 from nicegui import ui  # type: ignore
 
 
-def render(echart_file: str) -> None:
-    @ui.refreshable
-    def render_echart(echart_file: str) -> None:
-        echart_option = hjson.loads(open(echart_file).read())
+@ui.page("/")
+def index():
+    echart_option = hjson.loads(open("echart_graph.json").read())
+
+    with ui.element("div").style("width: fit-content; height: 100vh; margin: auto;"):
         ui.echart(echart_option).style("width: 100vw; height: 100vh")
-
-    render_echart(echart_file)
-
-    ui.button("Refresh", on_click=render_echart.refresh).style(
-        "position: absolute; top: 10px; right: 10px"
-    )
-
-
-with ui.element("div").style("width: fit-content; height: 100vh; margin: auto;"):
-    render("echart_graph.json")
 
 
 if __name__ in {"__main__", "__mp_main__"}:
