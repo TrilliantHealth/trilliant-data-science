fcba0c216003c4ce790727a8f689b58e4077a0bc
Rueben Banalagay <rueben.banalagay@trillianthealth.com>
2025-06-17 11:32:14 -0500

    Merge branch 'main' into bugfix/unified-asset/display-cancer-on-oncology-attempt2
    GitOrigin-RevId: 7265b9ce194547a96bf89c4db7aeb26610d5867f

--- libs/adls/src/thds/adls/tools/upload.py
+++ libs/adls/src/thds/adls/tools/upload.py
@@ -1,16 +1,17 @@
 import argparse
 from pathlib import Path
 
-from thds.adls import cached, uri
+from thds.adls.cached_up_down import upload_through_cache
+from thds.adls.uri import resolve_uri
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", type=Path, help="A local file you want to upload.")
-    parser.add_argument("uri", type=uri.resolve_uri, help="A fully qualified path to an ADLS location")
+    parser.add_argument("uri", type=resolve_uri, help="A fully qualified path to an ADLS location")
     args = parser.parse_args()
 
-    cached.upload_through_cache(args.uri, args.path)
+    upload_through_cache(args.uri, args.path)
 
 
 if __name__ == "__main__":
