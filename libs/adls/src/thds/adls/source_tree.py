d3587f66cd0309fae4e32ac58f0a064c71f4caac
carias <carlos.arias@trillianthealth.com>
2025-07-14 13:33:33 -0400

    Merge branch 'main' into feature/data-updates
    GitOrigin-RevId: 53d8ed573f5c2b9be0923ee965f71a48b370cbd8

--- libs/adls/src/thds/adls/source_tree.py
+++ libs/adls/src/thds/adls/source_tree.py
@@ -6,25 +6,21 @@ from azure.storage.blob import BlobProperties, ContainerClient
 from thds.core import hashing
 from thds.core.source.tree import SourceTree
 
-from . import fqn, global_client, source, uri
+from . import fqn, global_client, hashes, source, uri
 
 
 @dataclass
 class BlobMeta:
     path: str
     size: int
-    md5: ty.Optional[hashing.Hash]
+    hash: ty.Optional[hashing.Hash]
 
 
 def to_blob_meta(blob_props: BlobProperties) -> BlobMeta:
     return BlobMeta(
         blob_props.name,
         blob_props.size,
-        (
-            hashing.Hash("md5", bytes(blob_props.content_settings.content_md5))
-            if blob_props.content_settings.content_md5
-            else None
-        ),
+        next(iter(hashes.extract_hashes_from_props(blob_props).values()), None),
     )
 
 
@@ -34,10 +30,11 @@ def to_blob_meta(blob_props: BlobProperties) -> BlobMeta:
 def list_blob_meta(
     container_client: ContainerClient, root_dir: str, match_suffix: str = ""
 ) -> ty.List[BlobMeta]:
-    """Gets the path (relative to the SA/container root), size, and MD5 hash of all blobs in a directory."""
+    """Gets the path (relative to the SA/container root), size, and _a_ hash (if available) of all blobs in a directory."""
     return [
         to_blob_meta(blob_props)
-        for blob_props in container_client.list_blobs(name_starts_with=root_dir)
+        for blob_props in container_client.list_blobs(name_starts_with=root_dir, include=["metadata"])
+        # `list_blobs` does not include metadata by default, so we need to explicitly specify including it
         if blob_props.size > 0
         # container client lists directories as blobs with size 0
         and blob_props.name.endswith(match_suffix)
@@ -54,7 +51,7 @@ def from_path(adls_path: uri.UriIsh, match_suffix: str = "") -> SourceTree:
     container_root = root_fqn.root()
     return SourceTree(
         sources=[
-            source.from_adls(container_root / blob_meta.path, hash=blob_meta.md5)
+            source.from_adls(container_root / blob_meta.path, hash=blob_meta.hash)
             for blob_meta in list_blob_meta(container_client, root_fqn.path, match_suffix=match_suffix)
         ],
         higher_logical_root=fqn.split(root_fqn)[-1],
