fe7e4cd9a58bd078bb022f6daa4beeb834207a87
Grant Andersen <gandersen.codes@gmail.com>
2025-07-15 15:46:04 +0000

    Merge pull request #2979 from TrilliantHealth/bugfix/adls-source-md5-not-converted-from-bytearray
    convert content_settings md5 bytearray to bytes in extract_hashes_fro…
    
    GitOrigin-RevId: 2afa8713b3f2831f01eb59b3d41f17e942cf0941

--- libs/adls/tests/test_adls/test_hashes.py
+++ libs/adls/tests/test_adls/test_hashes.py
@@ -1,7 +1,24 @@
+import hashlib
 import os
 import tempfile
+import typing as ty
+
+from attrs import define
 
 from thds.adls import hashes
+from thds.core.hashing import Hash
+
+
+@define
+class FakeContentSettings:
+    content_md5: ty.Optional[bytearray]
+
+
+@define
+class FakeFileProperties:
+    name: str
+    metadata: dict
+    content_settings: FakeContentSettings
 
 
 def test_basic_preferred_hash():
@@ -11,3 +28,18 @@ def test_basic_preferred_hash():
         temp_file_path = temp_file.name
         # Calculate the hash using the preferred algorithm
         assert hashes.hash_cache.filehash(hashes.PREFERRED_ALGOS[0], temp_file_path) is not None
+
+
+def test_extract_hashes_from_props_with_md5_only():
+    m = hashlib.md5()
+    m.update("abc".encode("utf-8"))
+    md5_bytes = m.digest()
+
+    props = FakeFileProperties(
+        "a_file",
+        metadata=dict(),
+        content_settings=FakeContentSettings(content_md5=bytearray(md5_bytes)),
+    )
+    assert hashes.extract_hashes_from_props(props) == {
+        "md5": Hash("md5", md5_bytes),
+    }
