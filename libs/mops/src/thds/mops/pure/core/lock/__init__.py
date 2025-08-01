93c66ec4c6d070e37505928bcf642da2d6cd7c17
Rueben Banalagay <rueben.banalagay@trillianthealth.com>
2025-08-01 07:33:29 -0500

    Merge branch 'feature/data-updates' into feature/reference-data/update-esri
    GitOrigin-RevId: cbb38351c0d674b5cb55e6782c383e35d35f111a

--- libs/mops/src/thds/mops/pure/core/lock/__init__.py
+++ libs/mops/src/thds/mops/pure/core/lock/__init__.py
@@ -1,7 +1,6 @@
 from ._acquire import acquire  # noqa: F401
 from .maintain import (  # noqa: F401
     CannotMaintainLock,
-    LockWasStolenError,
     launch_daemon_lock_maintainer,
     maintain_to_release,
     remote_lock_maintain,
