cc55bb99e3a8e10b574bcd8706efd7092a1a1103
ruebenbanalagay <119341332+ruebenbanalagay@users.noreply.github.com>
2025-08-12 17:09:47 -0500

    Merge branch 'main' into feature/drg-imputation/procedure-subtype-consistency
    GitOrigin-RevId: 6d321417a27f76e37c020a3a2a56e58fa4cd0a9a

--- libs/core/src/thds/core/iterators.py
+++ libs/core/src/thds/core/iterators.py
@@ -1,4 +1,6 @@
+import time
 import typing as ty
+from datetime import timedelta
 
 T = ty.TypeVar("T")
 
@@ -7,3 +9,20 @@ def null_safe_iter(it: ty.Optional[ty.Iterable[T]]) -> ty.Iterator[T]:
     """Iterate the iterable if it is not None"""
     if it is not None:
         yield from it
+
+
+_Frequency = ty.Union[timedelta, float]
+
+
+def _to_float(t: _Frequency) -> float:
+    return t.total_seconds() if isinstance(t, timedelta) else t
+
+
+def titrate(it: ty.Iterable[T], *, at_rate: _Frequency, until_nth: int) -> ty.Iterator[T]:
+    it_, freq = iter(it), _to_float(at_rate)
+
+    for i, x in enumerate(it_):
+        yield x
+
+        if i + 1 < until_nth:
+            time.sleep(freq)
