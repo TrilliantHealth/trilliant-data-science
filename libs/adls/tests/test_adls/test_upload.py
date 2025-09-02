0fbbf331282f5b367d892066e48a2283b7de8647
Cailey Kerley <cailey.kerley@trillianthealth.com>
2025-09-02 11:01:22 -0400

    Merge branch 'main' into feature/purple-models-ud-migration
    GitOrigin-RevId: 6a24a55221a284f6716189644a272bdb3414b5f9

--- libs/adls/tests/test_adls/test_upload.py
+++ libs/adls/tests/test_adls/test_upload.py
@@ -1,16 +1,11 @@
-import io
 import logging
 import tempfile
-import typing as ty
-from datetime import datetime
 from pathlib import Path
 from random import randint
 
 from thds.adls import ADLSFileSystem, hashes
 from thds.adls._upload import upload_decision_and_metadata
 from thds.adls.global_client import get_global_fs_client
-from thds.adls.upload import upload
-from thds.core import hashing
 
 HW = Path(__file__).parent.parent / "data/hello_world.txt"
 
@@ -76,51 +71,3 @@ def test_cached_large_upload(caplog):
         with caplog.at_level(logging.INFO):
             fs.put_file(local_path, dest_path)  # this should get skipped after checksum calc.
             assert "already exists and has matching checksum" in caplog.text
-
-
-now = datetime.now()
-
-
-class Test_upload:
-    def test_upload_path(self, tmp_path, request):
-        src = Path(tmp_path / "1.txt")
-        src.write_text("1234")
-
-        uploaded = upload(f"adls://thdsscratch/vtmp/test/adls/upload/{now}-{request.node.name}.txt", src)
-
-        assert uploaded.hash
-        assert uploaded.hash.bytes == hashing.hash_using(src, hashes.default_hasher()).digest()
-
-    def test_upload_bytes(self, request):
-        bytes_ = "1234".encode()
-
-        uploaded = upload(
-            f"adls://thdsscratch/vtmp/test/adls/upload/{now}-{request.node.name}.txt", bytes_
-        )
-
-        assert uploaded.hash
-        assert uploaded.hash.bytes == hashing.hash_using(bytes_, hashes.default_hasher()).digest()
-
-    def test_upload_bytes_io(self, request):
-        buffer = io.BytesIO(b"1234")
-        uploaded = upload(
-            f"adls://thdsscratch/vtmp/test/adls/upload/{now}-{request.node.name}.txt", buffer
-        )
-
-        buffer.seek(0)
-        expected_hash = hashing.hash_using(buffer, hashes.default_hasher()).digest()
-        assert uploaded.hash and uploaded.hash.bytes == expected_hash
-
-    def test_upload_iter_bytes(self, request):
-        def _yield_bytes() -> ty.Iterator[bytes]:
-            for i in range(1, 5):
-                yield str(i).encode()
-
-        uploaded = upload(
-            f"adls://thdsscratch/vtmp/test/adls/upload/{now}-{request.node.name}.txt", _yield_bytes()
-        )
-
-        assert uploaded.hash is None
-        # we can't take a hash of an Iterator, because it will consume it pre-upload
-
-        assert uploaded.path().read_bytes() == b"".join(_yield_bytes())
