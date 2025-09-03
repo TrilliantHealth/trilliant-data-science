f042636b78d8c98716a320cc6bf50bfb9a3fd351
Cailey Kerley <cailey.kerley@trillianthealth.com>
2025-09-03 12:14:46 -0400

    Merge branch 'main' into bugfix/make-tests-require-all-results
    GitOrigin-RevId: d3290c7e926a628c292d2bc810d625a0018f365a

--- libs/mops/src/thds/mops/pure/core/serialize_paths.py
+++ libs/mops/src/thds/mops/pure/core/serialize_paths.py
@@ -87,19 +87,9 @@ def _serialize_file_path_as_upload(
     once: once.Once, path_keyer: _ProcessLockingPathContentAddresser, stream: PathStream, local_src: Path
 ) -> ty.Optional[Downloader]:
     if not local_src.exists():
-        if not local_src.is_absolute():
-            return None  # relative paths that don't exist will be serialized as plain relative paths.
-
-        raise NotAFileError(
-            f"You asked mops to upload the absolute Path {local_src}, but it does not exist."
-        )
-
+        raise NotAFileError(f"You asked mops to upload the path {local_src}, but it does not exist.")
     if not local_src.is_file():
-        if not local_src.is_absolute():
-            return None  # relative paths that aren't files will be serialized as plain relative paths.
-        raise NotAFileError(
-            f"You asked mops to upload the absolute Path {local_src}, but it is not a file."
-        )
+        raise NotAFileError(f"You asked mops to upload the Path {local_src}, but it is not a file.")
 
     remote_root = path_keyer(local_src)
     # I am creating a root 'directory' so that we can put debug info
--- libs/mops/tests/test_mops/integration/pure/path_test.py
+++ libs/mops/tests/test_mops/integration/pure/path_test.py
@@ -12,7 +12,6 @@ from ._util import adls_shim
 def _consume_a_path_and_return_one(path: Path) -> Path:
     tmp_out = tempdir() / "out"
     with open(path) as p:
-        print("reversing contents of file and writing to tmp out path:", tmp_out)
         with open(tmp_out, "w") as w:
             # reverse the bytes of the file:
             w.write(p.read()[::-1])
@@ -39,24 +38,3 @@ def test_that_normal_path_works():
     out_pth = _consume_a_path_and_return_one(pth)
     with open(out_pth) as r:
         assert r.read() == "dlrow olleh"
-
-
-@adls_shim
-def _write_foo_into_path(pth: Path) -> Path:
-    print(f"Will write to: {pth}")
-    with open(pth, "w") as w:
-        w.write("foo")
-    return pth
-
-
-def test_that_relative_path_does_not_error_if_not_file():
-    # relative paths that don't exist or aren't files are serialized as relative paths
-    try:
-        not_a_file = Path("not-a-file")
-        assert not not_a_file.is_absolute()
-        not_a_file.unlink(missing_ok=True)
-        out_pth = _write_foo_into_path(not_a_file)
-        assert not_a_file.exists()  # we did actually write to this file!
-        assert out_pth.read_text() == "foo"
-    finally:
-        not_a_file.unlink()
