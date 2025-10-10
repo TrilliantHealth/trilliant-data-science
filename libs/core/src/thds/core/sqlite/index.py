12e4c490d42e11562ab89ae8cf0344193c5ddda7
Carlos Arias <carlos.arias@trillianthealth.com>
2025-10-10 10:44:47 -0400

    Merge branch 'main' into feature/models_visits_refactor_downstream
    GitOrigin-RevId: 8b96ec688aede0d668c0e92d8b7b3bb45d9c383e

--- libs/core/src/thds/core/sqlite/index.py
+++ libs/core/src/thds/core/sqlite/index.py
@@ -4,19 +4,23 @@ from .connect import Connectable, autoconn_scope, autoconnect
 
 
 @autoconn_scope.bound
-def create(connectable: Connectable, table_name: str, columns: ty.Collection[str], unique: bool = False):
-    """Is idempotent, but does not verify that your index DDL matches what you're asking for."""
-    conn = autoconnect(connectable)
-    """Create an index on a table in a SQLite database using only sqlite3 and SQL."""
+def create(
+    connectable: Connectable, table_name: str, columns: ty.Collection[str], unique: bool = False
+) -> str:
+    """Create an index on a table in a SQLite database using only sqlite3 and SQL.
+
+    Is idempotent, but does not verify that your index DDL matches what you're asking for."""
     colnames = "_".join(colname for colname in columns).replace("-", "_")
+    idx_name = f"idx_{table_name}__{colnames}"
 
     sql_create_index = (
         f"CREATE {'UNIQUE' if unique else ''} INDEX IF NOT EXISTS "
-        f"[{table_name}_{colnames}_idx] ON [{table_name}] ({', '.join(columns)})"
+        f"[{idx_name}] ON [{table_name}] ({', '.join(columns)})"
     )
     try:
-        conn.execute(sql_create_index)
+        autoconnect(connectable).execute(sql_create_index)
         # do not commit - let the caller decide when to commit, or allow autoconnect to do its job
+        return idx_name
     except Exception:
-        print(sql_create_index)
+        print("FAILURE TO CREATE INDEX: " + sql_create_index)
         raise
