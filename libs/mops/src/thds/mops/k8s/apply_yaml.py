9c79f0e90c4604aea12c173cc517f411b740bfba
Carlos Arias <carlos.arias@trillianthealth.com>
2025-10-10 10:44:47 -0400

    Merge branch 'main' into feature/models_visits_refactor_downstream
    GitOrigin-RevId: 8b96ec688aede0d668c0e92d8b7b3bb45d9c383e

--- libs/mops/src/thds/mops/k8s/apply_yaml.py
+++ libs/mops/src/thds/mops/k8s/apply_yaml.py
@@ -1,10 +1,35 @@
+import subprocess
 import tempfile
 
+import kubernetes
 from kubernetes import client, utils
+from packaging import version
 
 
 def format_yaml(yaml_template_str: str, **template_values: str) -> str:
     return yaml_template_str.format(**template_values)
+
+
+def kubectl_apply_file(yaml_path: str) -> None:
+    subprocess.run(["kubectl", "apply", "-f", yaml_path], check=True)
+
+
+def kubectl_apply(yaml_string: str) -> None:
+    with tempfile.NamedTemporaryFile("w", prefix="kubectl-yaml") as f:
+        f.write(yaml_string)
+        f.flush()
+        kubectl_apply_file(f.name)
+
+
+def apply_yaml(yaml_path: str) -> None:
+    if version.parse(kubernetes.__version__) < version.parse("32.0.0"):
+        kubectl_apply_file(yaml_path)  # best effort
+        return
+
+    # NOTE: Prior to 32.0.0, this function doesn't actually server-side apply.
+    # https://github.com/kubernetes-client/python/pull/2252
+    # Hence the check above to use kubectl for older versions.
+    utils.create_from_yaml(client.ApiClient(), yaml_path, apply=True)
 
 
 def create_yaml_template(yaml_str: str, **template_values: str) -> None:
@@ -12,11 +37,9 @@ def create_yaml_template(yaml_str: str, **template_values: str) -> None:
 
     You must already have set up your SDK config.
 
-    NOTE: This function doesn't actually apply, and can't until the next release of the K8S SDK:
-    https://github.com/kubernetes-client/python/pull/2252
     """
     formatted_yaml = format_yaml(yaml_str, **template_values)
     with tempfile.NamedTemporaryFile("w", prefix="kubectl-yaml") as f:
         f.write(formatted_yaml)
         f.flush()
-        utils.create_from_yaml(client.ApiClient(), f.name)
+        apply_yaml(f.name)
